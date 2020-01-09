4eacaee7655a4c3836749fc84db4d2967600629b
Christopher Barnwell <christopher.barnwell@ibm.com>
2020-01-09 17:06:36 -0600

    feat(tutorial): complete step 1

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+	x = 1;
+	title = 'carbon-angular-tutorial';
+}
