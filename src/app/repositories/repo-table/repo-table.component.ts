1c105dc50d55803097cb249d58b213bd270e0ef6
JONASLALICAN <jonas.lalican-cic-it@ibm.com>
2020-01-17 11:49:02 +0100

    feat(tutorial): complete step 3

--- src/app/repositories/repo-table/repo-table.component.ts
+++ src/app/repositories/repo-table/repo-table.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, TemplateRef } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
 
 import {
 	TableModel,
 	TableItem,
-	TableHeaderItem
+	TableHeaderItem,
+	Table
 } from 'carbon-components-angular';
 
 @Component({
@@ -11,10 +14,20 @@ import {
 	templateUrl: './repo-table.component.html',
 	styleUrls: ['./repo-table.component.scss']
 })
-export class RepoTableComponent implements OnInit {
-	model = new TableModel();
 
-	constructor() { }
+export class RepoTableComponent implements OnInit {
+
+	model = new TableModel();
+	skeletonModel = Table.skeletonModel(10, 6);
+	skeleton = true;
+	data = [];
+
+	@ViewChild('linkTemplate', null)
+	protected linkTemplate: TemplateRef<any>;
+
+
+	constructor(private apollo: Apollo) { }
+
 
 	ngOnInit() {
 		this.model.data = [
@@ -43,6 +56,7 @@ export class RepoTableComponent implements OnInit {
 				new TableItem({data: 'Links'})
 			]
 		];
+
 		this.model.header = [
 			new TableHeaderItem({data: 'Name'}),
 			new TableHeaderItem({data: 'Created'}),
@@ -51,5 +65,90 @@ export class RepoTableComponent implements OnInit {
 			new TableHeaderItem({data: 'Stars'}),
 			new TableHeaderItem({data: 'Links'})
 		];
+
+		this.apollo.watchQuery({
+			query: gql`
+			  query REPO_QUERY {
+				# Let's use carbon as our organization
+				organization(login: "carbon-design-system") {
+				  # We'll grab all the repositories in one go. To load more resources
+				  # continuously, see the advanced topics.
+				  repositories(first: 75, orderBy: { field: UPDATED_AT, direction: DESC }) {
+					totalCount
+					nodes {
+					  url
+					  homepageUrl
+					  issues(filterBy: { states: OPEN }) {
+						totalCount
+					  }
+					  stargazers {
+						totalCount
+					  }
+					  releases(first: 1) {
+						totalCount
+						nodes {
+						  name
+						}
+					  }
+					  name
+					  updatedAt
+					  createdAt
+					  description
+					  id
+					}
+				  }
+				}
+			  }
+			`
+		}).valueChanges.subscribe((response: any) => {
+			if (response.error) {
+				const errorData = [];
+				errorData.push([
+					new TableItem({data: 'error!' })
+				]);
+				this.model.data = errorData;
+			} else if (response.loading) {
+				// Add loading state
+				this.skeleton = true;
+			} else {
+				// If we're here, we've got our data!
+				this.data = response.data.organization.repositories.nodes;
+				this.model.pageLength = 10;
+				this.model.totalDataLength = response.data.organization.repositories.totalCount;
+				this.selectPage(1);
+			}
+		});
+
 	}
+
+	prepareData(data) {
+		this.skeleton = false;
+		const newData = [];
+
+		for (const datum of data) {
+		newData.push([
+			new TableItem({ data: datum.name, expandedData: datum.description }),
+			new TableItem({ data: new Date(datum.createdAt).toLocaleDateString() }),
+			new TableItem({ data: new Date(datum.updatedAt).toLocaleDateString() }),
+			new TableItem({ data: datum.issues.totalCount }),
+			new TableItem({ data: datum.stargazers.totalCount }),
+			new TableItem({
+			data: {
+				github: datum.url,
+				homepage: datum.homepageUrl
+			},
+			template: this.linkTemplate
+			})
+		]);
+		}
+		return newData;
+	}
+
+	selectPage(page) {
+		const offset = this.model.pageLength * (page - 1);
+		const pageRawData = this.data.slice(offset, offset + this.model.pageLength);
+		this.model.data = this.prepareData(pageRawData);
+		this.model.currentPage = page;
+	}
+
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,6 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-	production: false
+	production: false,
+	githubPersonalAccessToken: 'f5671e16efea3ef49a99fa5e0bc070a26343b97c'
 };
