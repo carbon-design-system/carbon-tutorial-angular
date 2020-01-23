86224483a1d7346e34a76ddc968333e76f25722a
Lisa Koganezawa <lisa.koganezawa@novascotia.ca>
2020-01-23 13:30:29 -0400

    feat(tutorial): complete step 2

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,16 +1,24 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HeaderModule } from 'carbon-components-angular/ui-shell/ui-shell.module';
 
 import { HeaderComponent } from './header.component';
-import { UIShellModule } from 'carbon-components-angular';
+import { Notification20Module } from '@carbon/icons-angular/lib/notification/20';
+import { UserAvatar20Module } from '@carbon/icons-angular/lib/user--avatar/20';
+import { AppSwitcher20Module } from '@carbon/icons-angular/lib/app-switcher/20';
 
-describe('HeaderComponent', () => {
+describe('TutorialHeaderComponent', () => {
 	let component: HeaderComponent;
 	let fixture: ComponentFixture<HeaderComponent>;
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
 			declarations: [ HeaderComponent ],
-			imports: [ UIShellModule ]
+			imports: [
+				HeaderModule,
+				Notification20Module,
+				UserAvatar20Module,
+				AppSwitcher20Module
+			]
 		})
 		.compileComponents();
 	}));
