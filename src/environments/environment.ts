8e59e0b074e4b7b23341be83106c22824e2f01d7
Lisa Koganezawa <lisa.koganezawa@novascotia.ca>
2020-01-27 09:42:31 -0400

    feat(tutorial): complete step 3

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,6 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-	production: false
+	production: false,
+	githubPersonalAccessToken: '0ec7ca8e6300eece7a4fe3e08438901c66acc431',
 };
