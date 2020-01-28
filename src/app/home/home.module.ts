8319eb849667294fd57beb79314f511d3a1f21b1
Ahmed Nouira <ahmnouira@gmail.com>
2020-01-28 01:01:26 +0100

    feat(tutorial): complete step 2

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,11 +4,17 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 
+import { ButtonModule, BreadcrumbModule, GridModule, TabsModule } from 'carbon-components-angular';
+
 @NgModule({
 	declarations: [LandingPageComponent],
 	imports: [
 		CommonModule,
-		HomeRoutingModule
+		HomeRoutingModule,
+		ButtonModule,
+		BreadcrumbModule,
+		GridModule,
+		TabsModule
 	]
 })
 export class HomeModule { }
--- src/app/home/landing-page/landing-page.component.spec.ts
+++ src/app/home/landing-page/landing-page.component.spec.ts
@@ -2,15 +2,18 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LandingPageComponent } from './landing-page.component';
 
+import { ButtonModule, BreadcrumbModule, GridModule, TabsModule } from 'carbon-components-angular';
+
 describe('LandingPageComponent', () => {
 	let component: LandingPageComponent;
 	let fixture: ComponentFixture<LandingPageComponent>;
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
-			declarations: [ LandingPageComponent ]
+			declarations: [LandingPageComponent],
+			imports: [ButtonModule, BreadcrumbModule, GridModule, TabsModule]
 		})
-		.compileComponents();
+			.compileComponents();
 	}));
 
 	beforeEach(() => {
