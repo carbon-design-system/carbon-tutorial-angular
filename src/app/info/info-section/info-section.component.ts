b2da2574298ca45eeaf0dad6b80b35be187f62e0
Matthew Poje <matthew.poje@junxure.net>
2020-01-30 17:47:00 -0500

    feat(tutorial): complete step 5

--- src/app/info/info-section/info-section.component.ts
+++ src/app/info/info-section/info-section.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import * as data from './../info.json';
 
 @Component({
 	selector: 'app-info-section',
 	templateUrl: './info-section.component.html',
-	styleUrls: ['./info-section.component.scss']
+	styleUrls: ['./info-section.component.scss'],
 })
 export class InfoSectionComponent {
 	heading = data.title;
