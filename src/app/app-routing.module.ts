69a768c18a121a5cc7ce0f36914654d3df7f137f
Prateema Sharma <prateem1@in.ibm.com>
2020-02-03 18:01:32 +0530

    Angular step 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
 	{
-		path: '',
-		loadChildren: () => import('./starter-home/starter-home.module').then(m => m.StarterHomeModule)
-	}
-];
+	path: '',
+	loadChildren: () => import('./home/home.module').then(m => m.HomeModule),
+	},
+	{
+	path: 'repos',
+	loadChildren: () =>
+		import('./repositories/repositories.module').then(
+		m => m.RepositoriesModule
+		),
+	},
+	];
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
