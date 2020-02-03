aff3d6646e5a9b20b308aeda3abed671634c522d
Prateema Sharma <prateem1@in.ibm.com>
2020-02-03 19:31:18 +0530

    feat(tutorial): complete step 2

--- src/app/repositories/repo-page/repo-page.component.spec.ts
+++ src/app/repositories/repo-page/repo-page.component.spec.ts
@@ -1,14 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { GridModule, TableModule } from 'carbon-components-angular';
 
 import { RepoPageComponent } from './repo-page.component';
-
+import { RepoTableComponent } from '../repo-table/repo-table.component';
 describe('RepoPageComponent', () => {
 	let component: RepoPageComponent;
 	let fixture: ComponentFixture<RepoPageComponent>;
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
-			declarations: [ RepoPageComponent ]
+			declarations: [ RepoPageComponent, RepoTableComponent ],
+			imports: [GridModule, TableModule]
 		})
 		.compileComponents();
 	}));
