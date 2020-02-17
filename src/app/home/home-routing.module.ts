2d75e1f0ad10806dbfc32fd39d3fd16debdd6309
Manuel Eljuri <manueleljuri@gmail.com>
2020-02-16 20:22:55 -0600

    repair

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,10 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-<<<<<<< HEAD
-
-
-const routes: Routes = [];
-=======
 import { LandingPageComponent } from './landing-page/landing-page.component';
 
 
@@ -14,7 +9,6 @@ const routes: Routes = [
 		component: LandingPageComponent,
 	}
 ];
->>>>>>> 0e0c6ef63175c81fab6dc8e9fbd5ef27ae43335f
 
 @NgModule({
 	imports: [RouterModule.forChild(routes)],
