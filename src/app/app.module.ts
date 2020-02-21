e3cda55e1d67bae880b147abc9dac6312053d62c
Gabriel Barbosa <gabriel.b.oliveira@ibm.com>
2020-02-21 14:21:34 -0300

    feat(tutorial): complete step 1

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppSwitcher20Module } from '@carbon/icons-angular/lib/app-switcher/20';
+import { Notification20Module } from '@carbon/icons-angular/lib/notification/20';
+import { UserAvatar20Module } from '@carbon/icons-angular/lib/user--avatar/20';
+// carbon-components-angular default imports
+import { UIShellModule } from 'carbon-components-angular';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+
+
+
+@NgModule({
+	declarations: [
+		AppComponent,
+		HeaderComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		UIShellModule,
+		Notification20Module,
+		UserAvatar20Module,
+		AppSwitcher20Module
+	],
+	bootstrap: [AppComponent]
+})
+export class AppModule { }
