4952095a8d59d2855732e0936ff75a6303c7576a
leonardo.sr.92@gmail.com <leoreis@br.ibm.com>
2020-02-24 09:43:49 -0300

    feat(tutorial): complete step 4

--- src/app/info/info-card/info-card.component.ts
+++ src/app/info/info-card/info-card.component.ts
@@ -8,9 +8,17 @@ import { Component, OnInit, Input } from '@angular/core';
 export class InfoCardComponent implements OnInit {
 	@Input() heading;
 	@Input() content;
+	splitHeading;
 	constructor() { }
 
 	ngOnInit() {
+		this.splitHeading = this.createArrayFromPhrase(this.heading);
 	}
+	
+	createArrayFromPhrase(phrase) {
+		const splitPhrase = phrase.split(" ");
+		const thirdWord = splitPhrase.pop();
+		return [splitPhrase.join(" "), thirdWord];
+	  }
 
 }
