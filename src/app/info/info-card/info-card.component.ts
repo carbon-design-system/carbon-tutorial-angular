4a7d7c73051bd561a7d8c444ee54147df30ce70d
leonardo.sr.92@gmail.com <leoreis@br.ibm.com>
2020-02-25 14:55:16 -0300

    feat(tutorial): complete step 4

--- src/app/info/info-card/info-card.component.ts
+++ src/app/info/info-card/info-card.component.ts
@@ -9,16 +9,16 @@ export class InfoCardComponent implements OnInit {
 	@Input() heading;
 	@Input() content;
 	splitHeading;
-	constructor() { }
 
 	ngOnInit() {
+		// create the split title to get bold styles
 		this.splitHeading = this.createArrayFromPhrase(this.heading);
 	}
-	
-	createArrayFromPhrase(phrase) {
-		const splitPhrase = phrase.split(" ");
-		const thirdWord = splitPhrase.pop();
-		return [splitPhrase.join(" "), thirdWord];
-	  }
 
+	// Take in a phrase and separate the third word in an array
+	createArrayFromPhrase(phrase) {
+		const splitPhrase = phrase.split(' ');
+		const thirdWord = splitPhrase.pop();
+		return [splitPhrase.join(' '), thirdWord];
+	}
 }
