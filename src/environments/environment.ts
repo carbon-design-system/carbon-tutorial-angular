d6077e9fbe1a9d0c9a7d84703185b1279c615b1f
Enoc Martinez <shanock.charras@gmail.com>
2020-02-28 15:02:42 -0600

    feat(tutorial): complete step 3

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,6 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-	production: false
+	production: false,
+	githubPersonalAccessToken: '58e7c7e8fdcd51c770f8f276783068b9cdbc3f19'
 };
