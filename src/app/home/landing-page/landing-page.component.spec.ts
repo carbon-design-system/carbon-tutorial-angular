603c5ebe48ff98e6978934237a9259b7b7cf4b93
Brittany <brittany.boren@ibm.com>
2020-03-17 11:41:04 -0500

    feat(tutorial): complete step 2

--- src/app/home/landing-page/landing-page.component.spec.ts
+++ src/app/home/landing-page/landing-page.component.spec.ts
@@ -2,13 +2,16 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LandingPageComponent } from './landing-page.component';
 
+import { GridModule, BreadcrumbModule, TabsModule, ButtonModule } from 'carbon-components-angular';
+
 describe('LandingPageComponent', () => {
 	let component: LandingPageComponent;
 	let fixture: ComponentFixture<LandingPageComponent>;
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
-			declarations: [ LandingPageComponent ]
+			declarations: [ LandingPageComponent ],
+			imports: [GridModule, BreadcrumbModule, TabsModule, ButtonModule]
 		})
 		.compileComponents();
 	}));
