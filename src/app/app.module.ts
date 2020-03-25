3fe58e83c56eeadf83b94d5174f396a636dc207b
Andrew Agnew <andrew.agnew@afgonline.com.au>
2020-03-25 23:09:28 +0800

    feat(tutorial): complete step 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 // carbon-components-angular default imports
 import { UIShellModule } from 'carbon-components-angular';
 import { Notification20Module } from '@carbon/icons-angular/lib/notification/20';
 import { UserAvatar20Module } from '@carbon/icons-angular/lib/user--avatar/20';
 import { AppSwitcher20Module } from '@carbon/icons-angular/lib/app-switcher/20';
-import { HeaderComponent } from './header/header.component';
+
 
 @NgModule({
 	declarations: [
