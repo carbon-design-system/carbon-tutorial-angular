1a2aa5914edd9baf99b701559a59cb38222f10f6
Ahmed Abd Elrehim <ahmed.farid1@ibm.com>
2020-04-13 17:48:39 +0200

    feat(tutorial): complete step 3

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,6 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-	production: false
+	production: false,
+	githubPersonalAccessToken: '1c8ea109768586b7e187f78bf0c05bb5ba857609'
 };
