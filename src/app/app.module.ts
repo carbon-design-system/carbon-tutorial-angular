d090826dab33144d913a8f3069181a4ace480689
Jasmine Smith <jasmsmit@au1.ibm.com>
2020-04-20 15:24:55 +1000

    feat(tutorial): complete step 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 // carbon-components-angular default imports
 import { UIShellModule } from 'carbon-components-angular';
-import { Notification20Module } from '@carbon/icons-angular/lib/notification/20';
-import { UserAvatar20Module } from '@carbon/icons-angular/lib/user--avatar/20';
-import { AppSwitcher20Module } from '@carbon/icons-angular/lib/app-switcher/20';
-import { HeaderComponent } from './header/header.component';
 
 @NgModule({
 	declarations: [
@@ -19,13 +15,8 @@ import { HeaderComponent } from './header/header.component';
 	],
 	imports: [
 		BrowserModule,
-		BrowserAnimationsModule,
-		FormsModule,
 		AppRoutingModule,
-		UIShellModule,
-		Notification20Module,
-		UserAvatar20Module,
-		AppSwitcher20Module
+		UIShellModule
 	],
 	bootstrap: [AppComponent]
 })
