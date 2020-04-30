8db19adf460cef782996cfffd2ad098eb2b41500
“ritu0407” <ritu0407@gmail.com>
2020-04-30 12:45:20 -0400

    feat(tutorial): complete step 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,22 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
 	{
-		path: '',
-		loadChildren: () => import('./starter-home/starter-home.module').then(m => m.StarterHomeModule)
-	}
-];
+	  path: '',
+	  loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+	},
+	{
+	  path: 'repos',
+	  loadChildren: () =>
+		import('./repositories/repositories.module').then(
+		  (m) => m.RepositoriesModule
+		),
+	},
+	];
 
-@NgModule({
-	imports: [RouterModule.forRoot(routes)],
-	exports: [RouterModule]
-})
-export class AppRoutingModule { }
+
+	@NgModule({
+	imports: [RouterModule.forRoot(routes, { useHash: true })],
+	exports: [RouterModule],
+	})
+	export class AppRoutingModule {}
+
