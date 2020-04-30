fcece20de4c03f56c64f375e46b4a3784c9b442f
veereshcb <veeresh_cb@yahoo.com>
2020-04-30 14:42:06 +0530

    feat(tutorial): complete step 2

---
+++ src/app/repositories/repo-table/repo-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-repo-table',
+  templateUrl: './repo-table.component.html',
+  styleUrls: ['./repo-table.component.scss']
+})
+export class RepoTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
