73f3b70a00de3f7731c0793bab517c146291f708
Cinthia Karen <cinthiakaren@MacBook-Pro-de-Cinthia.local>
2020-04-30 19:54:22 -0500

    fix styles

--- src/app/repositories/repo-table/repo-table.component.spec.ts
+++ src/app/repositories/repo-table/repo-table.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RepoTableComponent } from './repo-table.component';
+import { GridModule, TableModule } from 'carbon-components-angular';
 
 describe('RepoTableComponent', () => {
 	let component: RepoTableComponent;
@@ -8,7 +9,8 @@ describe('RepoTableComponent', () => {
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
-			declarations: [ RepoTableComponent ]
+			declarations: [ RepoTableComponent ],
+			imports: [GridModule, TableModule]
 		})
 		.compileComponents();
 	}));
--- src/app/repositories/repo-table/repo-table.component.ts
+++ src/app/repositories/repo-table/repo-table.component.ts
@@ -18,38 +18,38 @@ export class RepoTableComponent implements OnInit {
 	ngOnInit() {
 		this.model.data = [
 			[
-			  new TableItem({ data: 'Repo 1', expandedData: 'Row description' }),
-			  new TableItem({ data: 'Date' }),
-			  new TableItem({ data: 'Date' }),
-			  new TableItem({ data: '123' }),
-			  new TableItem({ data: '456' }),
-			  new TableItem({ data: 'Links' }),
+				new TableItem({ data: 'Repo 1', expandedData: 'Row description' }),
+				new TableItem({ data: 'Date' }),
+				new TableItem({ data: 'Date' }),
+				new TableItem({ data: '123' }),
+				new TableItem({ data: '456' }),
+				new TableItem({ data: 'Links' }),
 			],
 			[
-			  new TableItem({ data: 'Repo 2', expandedData: 'Row description' }),
-			  new TableItem({ data: 'Date' }),
-			  new TableItem({ data: 'Date' }),
-			  new TableItem({ data: '123' }),
-			  new TableItem({ data: '456' }),
-			  new TableItem({ data: 'Links' }),
+				new TableItem({ data: 'Repo 2', expandedData: 'Row description' }),
+				new TableItem({ data: 'Date' }),
+				new TableItem({ data: 'Date' }),
+				new TableItem({ data: '123' }),
+				new TableItem({ data: '456' }),
+				new TableItem({ data: 'Links' }),
 			],
 			[
-			  new TableItem({ data: 'Repo 3', expandedData: 'Row description' }),
-			  new TableItem({ data: 'Date' }),
-			  new TableItem({ data: 'Date' }),
-			  new TableItem({ data: '123' }),
-			  new TableItem({ data: '456' }),
-			  new TableItem({ data: 'Links' }),
+				new TableItem({ data: 'Repo 3', expandedData: 'Row description' }),
+				new TableItem({ data: 'Date' }),
+				new TableItem({ data: 'Date' }),
+				new TableItem({ data: '123' }),
+				new TableItem({ data: '456' }),
+				new TableItem({ data: 'Links' }),
 			],
-		  ];
-		  this.model.header = [
+		];
+		this.model.header = [
 			new TableHeaderItem({ data: 'Name' }),
 			new TableHeaderItem({ data: 'Created' }),
 			new TableHeaderItem({ data: 'Updated' }),
 			new TableHeaderItem({ data: 'Open Issues' }),
 			new TableHeaderItem({ data: 'Stars' }),
 			new TableHeaderItem({ data: 'Links' }),
-		  ];
+		];
 	}
 
 }
