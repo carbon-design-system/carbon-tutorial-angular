f339fee95ea4dc75a8068db4fb7f7a906d3697cb
Padmanabhan <pakosal1@in.ibm.com>
2020-05-04 22:11:38 +0530

    feat(tutorial): complete step 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
+import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { environment } from '../environments/environment';
+import { HttpHeaders, HttpClientModule } from '@angular/common/http';
+
 
 // carbon-components-angular default imports
 import { UIShellModule } from 'carbon-components-angular';
@@ -12,6 +18,8 @@ import { Notification20Module } from '@carbon/icons-angular/lib/notification/20'
 import { UserAvatar20Module } from '@carbon/icons-angular/lib/user--avatar/20';
 import { AppSwitcher20Module } from '@carbon/icons-angular/lib/app-switcher/20';
 import { HeaderComponent } from './header/header.component';
+import { GraphQLModule } from './graphql.module';
+
 
 @NgModule({
 	declarations: [
@@ -26,7 +34,10 @@ import { HeaderComponent } from './header/header.component';
 		UIShellModule,
 		Notification20Module,
 		UserAvatar20Module,
-		AppSwitcher20Module
+		AppSwitcher20Module,
+		ApolloModule,
+		GraphQLModule,
+		HttpClientModule
 	],
 	bootstrap: [AppComponent]
 })
--- src/app/repositories/repo-table/repo-table.component.ts
+++ src/app/repositories/repo-table/repo-table.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, TemplateRef } from '@angular/core';
 
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
 import {
+	Table,
 	TableModel,
 	TableItem,
 	TableHeaderItem
@@ -12,44 +15,110 @@ import {
 	styleUrls: ['./repo-table.component.scss']
 })
 export class RepoTableComponent implements OnInit {
-	model = new TableModel();
 
-	constructor() { }
+	model = Table.skeletonModel(2, 6);
+	skeleton = true;
+	data = [];
+
+
+	@ViewChild('linkTemplate', null) protected linkTemplate: TemplateRef<any>;
+
+	constructor(private apollo: Apollo) { }
 
 	ngOnInit() {
-		this.model.data = [
-			[
-				new TableItem({data: 'Repo 1', expandedData: 'Row description'}),
-				new TableItem({data: 'Date'}),
-				new TableItem({data: 'Date'}),
-				new TableItem({data: '123'}),
-				new TableItem({data: '456'}),
-				new TableItem({data: 'Links'})
-			],
-			[
-				new TableItem({data: 'Repo 2', expandedData: 'Row description'}),
-				new TableItem({data: 'Date'}),
-				new TableItem({data: 'Date'}),
-				new TableItem({data: '123'}),
-				new TableItem({data: '456'}),
-				new TableItem({data: 'Links'})
-			],
-			[
-				new TableItem({data: 'Repo 3', expandedData: 'Row description'}),
-				new TableItem({data: 'Date'}),
-				new TableItem({data: 'Date'}),
-				new TableItem({data: '123'}),
-				new TableItem({data: '456'}),
-				new TableItem({data: 'Links'})
-			]
-		];
+
+
 		this.model.header = [
-			new TableHeaderItem({data: 'Name'}),
-			new TableHeaderItem({data: 'Created'}),
-			new TableHeaderItem({data: 'Updated'}),
-			new TableHeaderItem({data: 'Open Issues'}),
-			new TableHeaderItem({data: 'Stars'}),
-			new TableHeaderItem({data: 'Links'})
+			new TableHeaderItem({ data: 'Name' }),
+			new TableHeaderItem({ data: 'Created' }),
+			new TableHeaderItem({ data: 'Updated' }),
+			new TableHeaderItem({ data: 'Open Issues' }),
+			new TableHeaderItem({ data: 'Stars' }),
+			new TableHeaderItem({ data: 'Links' })
 		];
+
+		this.apollo.watchQuery({
+			query: gql`
+			  query REPO_QUERY {
+				# Let's use carbon as our organization
+				organization(login: "carbon-design-system") {
+				  # We'll grab all the repositories in one go. To load more resources
+				  # continuously, see the advanced topics.
+				  repositories(first: 75, orderBy: { field: UPDATED_AT, direction: DESC }) {
+					totalCount
+					nodes {
+					  url
+					  homepageUrl
+					  issues(filterBy: { states: OPEN }) {
+						totalCount
+					  }
+					  stargazers {
+						totalCount
+					  }
+					  releases(first: 1) {
+						totalCount
+						nodes {
+						  name
+						}
+					  }
+					  name
+					  updatedAt
+					  createdAt
+					  description
+					  id
+					}
+				  }
+				}
+			  }
+			`
+		}).valueChanges.subscribe((response: any) => {
+			if (response.error) {
+				const errorData = [];
+				errorData.push([
+					new TableItem({ data: 'error!' })
+				]);
+				this.model.data = errorData;
+			} else if (response.loading) {
+				// Add loading state
+				this.skeleton = true;
+			} else {
+				this.skeleton = false;
+				// this.model.data = this.prepareData(response.data.organization.repositories.nodes);
+
+				this.data = response.data.organization.repositories.nodes;
+				this.model.pageLength = 10;
+				this.model.totalDataLength = response.data.organization.repositories.totalCount;
+				this.selectPage(1);
+
+			}
+		});
+	}
+
+	selectPage(page) {
+		const offset = this.model.pageLength * (page - 1);
+		const pageRawData = this.data.slice(offset, offset + this.model.pageLength);
+		this.model.data = this.prepareData(pageRawData);
+		this.model.currentPage = page;
+	}
+
+	prepareData(data) {
+		const newData = [];
+		for (const datum of data) {
+			newData.push([
+				new TableItem({ data: datum.name, expandedData: datum.description }),
+				new TableItem({ data: new Date(datum.createdAt).toLocaleDateString() }),
+				new TableItem({ data: new Date(datum.updatedAt).toLocaleDateString() }),
+				new TableItem({ data: datum.issues.totalCount }),
+				new TableItem({ data: datum.stargazers.totalCount }),
+				new TableItem({
+					data: {
+						github: datum.url,
+						homepage: datum.homepageUrl
+					},
+					template: this.linkTemplate
+				})
+			]);
+		}
+		return newData;
 	}
 }
