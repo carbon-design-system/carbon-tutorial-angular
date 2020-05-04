5bc1bb21a4d60cd98839e47b6f4fcdaedb940dab
Cinthia Karen <ckstgarcia@gmail.com>
2020-05-04 16:19:06 -0500

    fix ng lint

--- src/app/repositories/repositories.module.ts
+++ src/app/repositories/repositories.module.ts
@@ -3,10 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { RepositoriesRoutingModule } from './repositories-routing.module';
 import { RepoPageComponent } from './repo-page/repo-page.component';
-import {
-	GridModule,
-	TableModule,
-} from 'carbon-components-angular';
+import { GridModule, TableModule } from 'carbon-components-angular';
 import { RepoTableComponent } from './repo-table/repo-table.component';
 
 @NgModule({
