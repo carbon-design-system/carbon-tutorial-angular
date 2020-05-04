491f8d243a35d4ae57201d711112ceab49f6a9fc
Eric Rossi <eric.rossi@ibm.com>
2020-05-04 15:24:50 -0300

    feat(tutorial): complete step 3

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,7 +2,6 @@ import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { AppComponent } from './app.component';
-import { TutorialHeaderModule } from './tutorial-header/tutorial-header.module';
 
 describe('AppComponent', () => {
 	beforeEach(async(() => {
@@ -11,8 +10,7 @@ describe('AppComponent', () => {
 				AppComponent
 			],
 			imports: [
-				RouterTestingModule,
-				TutorialHeaderModule
+				RouterTestingModule
 			]
 		}).compileComponents();
 	}));
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,6 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-	production: false
-};
+	production: false,
+	githubPersonalAccessToken: '5a2cfaa3a2aaac511b9ab32434bdba53109cb911',
+	};
