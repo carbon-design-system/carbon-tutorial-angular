123505f968f5d54d334a7a361f8faa422b4c29d1
Cinthia Karen <ckstgarcia@gmail.com>
2020-05-05 21:43:43 -0500

    feat(tutorial): complete step 2

--- src/app/home/landing-page/landing-page.component.spec.ts
+++ src/app/home/landing-page/landing-page.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LandingPageComponent } from './landing-page.component';
+import { GridModule, BreadcrumbModule, TabsModule } from 'carbon-components-angular';
 
 describe('LandingPageComponent', () => {
 	let component: LandingPageComponent;
@@ -8,7 +9,8 @@ describe('LandingPageComponent', () => {
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
-			declarations: [ LandingPageComponent ]
+			declarations: [ LandingPageComponent ],
+			imports: [GridModule, BreadcrumbModule, TabsModule]
 		})
 		.compileComponents();
 	}));
