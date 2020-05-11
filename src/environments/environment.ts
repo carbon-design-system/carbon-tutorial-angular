2e8ea25b2db00f8fd87bd83ef17e37976b63b685
“ritu0407” <ritu0407@gmail.com>
2020-05-11 17:12:08 -0400

    feat(tutorial): complete step 5

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,5 @@
 
 export const environment = {
 	production: false,
-	githubPersonalAccessToken: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+	githubPersonalAccessToken: '*************************************'
 };
