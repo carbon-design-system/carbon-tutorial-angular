af57b95c1a263626555fbc8ea2d21ab8002df7b1
rfeng8 <rfeng8@volvocars.com>
2020-05-11 23:46:02 +0800

    feat(tutorial): complete step 3

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,6 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-	production: false
+	production: false,
+	githubPersonalAccessToken: '35420fd554a11a7e17283d32ba5f9373b7c931f3',
 };
