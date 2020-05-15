31cded837d481bd6a454d752f58dcac117f7b965
Anton Kanugalawattage <antonk@AK-IBM.local>
2020-05-15 12:20:30 -0400

    feat(tutorial): complete step 4

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,7 +2,7 @@ import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { AppComponent } from './app.component';
-import { TutorialHeaderModule } from './header/tutorial-header.module';
+import { HeaderComponent } from './header/header.component';
 
 describe('AppComponent', () => {
 	beforeEach(async(() => {
@@ -12,7 +12,7 @@ describe('AppComponent', () => {
 			],
 			imports: [
 				RouterTestingModule,
-				TutorialHeaderModule
+				HeaderComponent
 			]
 		}).compileComponents();
 	}));
