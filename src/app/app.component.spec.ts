bfec42c237004c3766cd3776870f7b16716ab612
steinagel <williansteinagel@gmail.com>
2020-05-21 14:20:22 -0300

    feat(tutorial): complete step 4

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,18 +2,16 @@ import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { AppComponent } from './app.component';
-import { TutorialHeaderModule } from './header/tutorial-header.module';
+// import { TutorialHeaderModule } from "./header/tutorial-header.module";
 
 describe('AppComponent', () => {
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
-			declarations: [
-				AppComponent
-			],
+			declarations: [AppComponent],
 			imports: [
 				RouterTestingModule,
-				TutorialHeaderModule
-			]
+				// TutorialHeaderModule
+			],
 		}).compileComponents();
 	}));
 
