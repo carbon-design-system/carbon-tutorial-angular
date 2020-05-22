83ab5e4d92a73e949323664f24238c702776bde2
AHTZIRI-FERNANDEZ1 <AHTZIRI.FERNANDEZ1@ibm.com>
2020-05-22 17:32:25 -0500

    feat(tutorial): complete step 2

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,14 +1,24 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { HomeRoutingModule } from './home-routing.module';
-import { LandingPageComponent } from './landing-page/landing-page.component';
+import { HomeRoutingModule } from "./home-routing.module";
+import { LandingPageComponent } from "./landing-page/landing-page.component";
+import {
+	BreadcrumbModule,
+	ButtonModule,
+	GridModule,
+	TabsModule,
+} from "carbon-components-angular";
 
 @NgModule({
 	declarations: [LandingPageComponent],
 	imports: [
 		CommonModule,
-		HomeRoutingModule
-	]
+		HomeRoutingModule,
+		GridModule,
+		BreadcrumbModule,
+		ButtonModule,
+		TabsModule,
+	],
 })
-export class HomeModule { }
+export class HomeModule {}
--- src/app/repositories/repo-page/repo-page.component.ts
+++ src/app/repositories/repo-page/repo-page.component.ts
@@ -1,9 +1,8 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-	selector: 'app-repo-page',
-	templateUrl: './repo-page.component.html',
-	styleUrls: ['./repo-page.component.scss']
+	selector: "app-repo-page",
+	templateUrl: "./repo-page.component.html",
+	styleUrls: ["./repo-page.component.scss"],
 })
-export class RepoPageComponent {
-}
+export class RepoPageComponent {}
