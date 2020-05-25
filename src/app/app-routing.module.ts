ac64c555ff8b30186d11cc7b30588fcf55c82627
Ritu Thakur <ritu.thakur@ibm.com>
2020-05-25 18:31:09 -0400

    Resolving conflicts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,5 +16,5 @@ const routes: Routes = [
 		imports: [RouterModule.forRoot(routes, { useHash: true })],
 	exports: [RouterModule],
 })
-export class AppRoutingModule {}
 
+export class AppRoutingModule {}
