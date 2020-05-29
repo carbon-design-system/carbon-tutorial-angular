ea5db84907fc40447ff7d7d1043b868c2c8d0652
Alexandre F Costa <afcosta@br.ibm.com>
2020-05-29 17:47:18 -0300

    feat(tutorial): complete step 2

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,9 +4,20 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 
+import {
+	BreadcrumbModule,
+	ButtonModule,
+	GridModule,
+	TabsModule,
+} from 'carbon-components-angular';
+
 @NgModule({
 	declarations: [LandingPageComponent],
 	imports: [
+		BreadcrumbModule,
+		ButtonModule,
+		GridModule,
+		TabsModule,
 		CommonModule,
 		HomeRoutingModule
 	]
--- src/app/repositories/repositories.module.ts
+++ src/app/repositories/repositories.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { RepositoriesRoutingModule } from './repositories-routing.module';
+
 import { RepoPageComponent } from './repo-page/repo-page.component';
+import { RepoTableComponent } from './repo-table/repo-table.component';
+
+import { GridModule, TableModule } from 'carbon-components-angular';
 
 @NgModule({
-	declarations: [RepoPageComponent],
+	declarations: [RepoPageComponent, RepoTableComponent],
 	imports: [
+		GridModule,
+		TableModule,
 		CommonModule,
 		RepositoriesRoutingModule
 	]
