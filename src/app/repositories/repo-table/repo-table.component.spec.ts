625485267a7b1bc6b97c32ede213712b22790493
Elizabeth Byrne <liz.judd@ibm.com>
2020-06-02 13:04:12 -0400

    feat(repo-table): dynamic data

--- src/app/repositories/repo-table/repo-table.component.spec.ts
+++ src/app/repositories/repo-table/repo-table.component.spec.ts
@@ -1,7 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 import { RepoTableComponent } from './repo-table.component';
-import { TableModule } from 'carbon-components-angular';
+import { GraphQLModule } from '../../graphql.module';
+import { TableModule, LinkModule, PaginationModule } from 'carbon-components-angular';
 
 describe('RepoTableComponent', () => {
 	let component: RepoTableComponent;
@@ -11,7 +13,11 @@ describe('RepoTableComponent', () => {
 		TestBed.configureTestingModule({
 			declarations: [ RepoTableComponent ],
 			imports: [
-				TableModule
+				TableModule,
+				LinkModule,
+				PaginationModule,
+				GraphQLModule,
+				HttpClientModule
 			]
 		})
 		.compileComponents();
