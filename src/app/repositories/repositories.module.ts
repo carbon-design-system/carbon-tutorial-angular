f02c3b24a65346394a2b94b8d4cd868e284ec3d2
Elizabeth Byrne <liz.judd@ibm.com>
2020-06-02 13:04:44 -0400

    test: update tests

--- src/app/repositories/repositories.module.ts
+++ src/app/repositories/repositories.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LinkModule, PaginationModule } from 'carbon-components-angular';
 
 import { RepositoriesRoutingModule } from './repositories-routing.module';
 import { RepoPageComponent } from './repo-page/repo-page.component';
@@ -12,7 +13,9 @@ import { RepoTableComponent } from './repo-table/repo-table.component';
 		CommonModule,
 		RepositoriesRoutingModule,
 		GridModule,
- 		TableModule
+		 TableModule,
+		 LinkModule,
+		 PaginationModule
 	]
 })
 export class RepositoriesModule { }
