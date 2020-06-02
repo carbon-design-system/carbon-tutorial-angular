0ad814394a78690af15b41f96e819208a5075a3e
AHTZIRI-FERNANDEZ1 <AHTZIRI.FERNANDEZ1@ibm.com>
2020-06-02 18:33:51 -0500

    feat(tutorial): complete step 2

--- src/app/repositories/repo-page/repo-page.component.spec.ts
+++ src/app/repositories/repo-page/repo-page.component.spec.ts
@@ -1,9 +1,9 @@
-import { async, ComponentFixture, TestBed } from "@angular/core/testing";
-import { RepoPageComponent } from "./repo-page.component";
-import { GridModule, TableModule } from "carbon-components-angular";
-import { RepoTableComponent } from "../repo-table/repo-table.component";
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RepoPageComponent } from './repo-page.component';
+import { GridModule, TableModule } from 'carbon-components-angular';
+import { RepoTableComponent } from '../repo-table/repo-table.component';
 
-describe("RepoPageComponent", () => {
+describe('RepoPageComponent', () => {
 	let component: RepoPageComponent;
 	let fixture: ComponentFixture<RepoPageComponent>;
 
@@ -20,7 +20,7 @@ describe("RepoPageComponent", () => {
 		fixture.detectChanges();
 	});
 
-	it("should create", () => {
+	it('should create', () => {
 		expect(component).toBeTruthy();
 	});
 });
--- src/app/repositories/repo-table/repo-table.component.ts
+++ src/app/repositories/repo-table/repo-table.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
 import {
 	TableModel,
 	TableItem,
 	TableHeaderItem,
-} from "carbon-components-angular";
+} from 'carbon-components-angular';
 
 @Component({
-	selector: "app-repo-table",
-	templateUrl: "./repo-table.component.html",
-	styleUrls: ["./repo-table.component.scss"],
+	selector: 'app-repo-table',
+	templateUrl: './repo-table.component.html',
+	styleUrls: ['./repo-table.component.scss'],
 })
 export class RepoTableComponent implements OnInit {
 	model = new TableModel();
@@ -20,45 +20,45 @@ export class RepoTableComponent implements OnInit {
 		this.model.data = [
 			[
 				new TableItem({
-					data: "Repo 1",
-					expandedData: "Row description",
+					data: 'Repo 1',
+					expandedData: 'Row description',
 				}),
-				new TableItem({ data: "Date" }),
-				new TableItem({ data: "Date" }),
-				new TableItem({ data: "123" }),
-				new TableItem({ data: "456" }),
-				new TableItem({ data: "Links" }),
+				new TableItem({ data: 'Date' }),
+				new TableItem({ data: 'Date' }),
+				new TableItem({ data: '123' }),
+				new TableItem({ data: '456' }),
+				new TableItem({ data: 'Links' }),
 			],
 			[
 				new TableItem({
-					data: "Repo 2",
-					expandedData: "Row description",
+					data: 'Repo 2',
+					expandedData: 'Row description',
 				}),
-				new TableItem({ data: "Date" }),
-				new TableItem({ data: "Date" }),
-				new TableItem({ data: "123" }),
-				new TableItem({ data: "456" }),
-				new TableItem({ data: "Links" }),
+				new TableItem({ data: 'Date' }),
+				new TableItem({ data: 'Date' }),
+				new TableItem({ data: '123' }),
+				new TableItem({ data: '456' }),
+				new TableItem({ data: 'Links' }),
 			],
 			[
 				new TableItem({
-					data: "Repo 3",
-					expandedData: "Row description",
+					data: 'Repo 3',
+					expandedData: 'Row description',
 				}),
-				new TableItem({ data: "Date" }),
-				new TableItem({ data: "Date" }),
-				new TableItem({ data: "123" }),
-				new TableItem({ data: "456" }),
-				new TableItem({ data: "Links" }),
+				new TableItem({ data: 'Date' }),
+				new TableItem({ data: 'Date' }),
+				new TableItem({ data: '123' }),
+				new TableItem({ data: '456' }),
+				new TableItem({ data: 'Links' }),
 			],
 		];
 		this.model.header = [
-			new TableHeaderItem({ data: "Name" }),
-			new TableHeaderItem({ data: "Created" }),
-			new TableHeaderItem({ data: "Updated" }),
-			new TableHeaderItem({ data: "Open Issues" }),
-			new TableHeaderItem({ data: "Stars" }),
-			new TableHeaderItem({ data: "Links" }),
+			new TableHeaderItem({ data: 'Name' }),
+			new TableHeaderItem({ data: 'Created' }),
+			new TableHeaderItem({ data: 'Updated' }),
+			new TableHeaderItem({ data: 'Open Issues' }),
+			new TableHeaderItem({ data: 'Stars' }),
+			new TableHeaderItem({ data: 'Links' }),
 		];
 	}
 }
