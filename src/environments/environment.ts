91577fbb205f2ecafb5dcb72b94677756265c251
ROGER ANTONIO CUEVAS BELANDRIA <Roger.cuevas@ibm.com>
2020-06-03 16:53:43 -0500

    feat(tutorial): complete step 3

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,6 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-	production: false
+	production: false,
+	githubPersonalAccessToken: '9c9b8c21bae558a618a0a9456c1dc76f1052da02'
 };
