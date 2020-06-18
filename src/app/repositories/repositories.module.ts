60a6b85f97437651fb3dcc14c880e6f4d215dba9
Navil <rodriguesnavil@gmail.com>
2020-06-18 10:47:26 +0530

    feat(tutorial): complete step 2

--- src/app/repositories/repositories.module.ts
+++ src/app/repositories/repositories.module.ts
@@ -4,14 +4,15 @@ import { CommonModule } from '@angular/common';
 import { RepositoriesRoutingModule } from './repositories-routing.module';
 import { RepoPageComponent } from './repo-page/repo-page.component';
 import { GridModule, TableModule } from 'carbon-components-angular';
+import { RepoTableComponent } from './repo-table/repo-table.component';
 
 @NgModule({
-	declarations: [RepoPageComponent],
+	declarations: [RepoPageComponent, RepoTableComponent],
 	imports: [
 		CommonModule,
 		RepositoriesRoutingModule,
 		GridModule,
-		TableModule
+		TableModule,
 	]
 })
 export class RepositoriesModule { }
