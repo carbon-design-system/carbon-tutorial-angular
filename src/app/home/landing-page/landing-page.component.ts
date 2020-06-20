0a0baae0062d1b18da008a61622f24ab0cd1321a
Jacob DeCicco <jacobdecicco@Jacobs-MBP.lan>
2020-06-20 19:16:29 -0400

    feat(tutorial): complete step 2

--- src/app/home/landing-page/landing-page.component.ts
+++ src/app/home/landing-page/landing-page.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
 	selector: 'app-landing-page',
 	templateUrl: './landing-page.component.html',
-	styleUrls: ['./landing-page.component.scss']
+	styleUrls: ['./landing-page.component.scss'],
 })
 export class LandingPageComponent {
 }
