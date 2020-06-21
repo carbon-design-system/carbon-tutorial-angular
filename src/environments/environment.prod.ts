9e75402f879f33eeb8f70111b9b9c0dacd0baa91
Jacob DeCicco <jacobdecicco@Jacobs-MBP.lan>
2020-06-20 22:50:57 -0400

    feat(tutorial): complete step 5

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
 	production: true,
-	githubPersonalAccessToken: '2d1d6f1914d46d3570b7ce8b2c7d7d12939f1a7f'
+	githubPersonalAccessToken: '3511873270ae93b3d9913ef4a7f67570472dce9f'
 };
