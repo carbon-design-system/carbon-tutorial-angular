3f215fbde9c1cfd1bbd4d56d83c8f0279b949c10
Lehi Orlando Del Bosque Muñoz <Lehi.Orlando.Bosque@ibm.com>
2020-06-21 16:39:19 -0500

    feat(tutorial): complete step 1

---
+++ src/app/home/home-routing.module.ts
@@ -0,0 +1,27 @@
+// import { NgModule } from '@angular/core';
+// import { Routes, RouterModule } from '@angular/router';
+//
+//
+// const routes: Routes = [];
+//
+// @NgModule({
+// 	imports: [RouterModule.forChild(routes)],
+// 	exports: [RouterModule]
+// })
+// export class HomeRoutingModule { }
+
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+const routes: Routes = [
+{
+	path: '',
+	component: LandingPageComponent,
+},
+];
+
+@NgModule({
+	imports: [RouterModule.forChild(routes)],
+	exports: [RouterModule]
+})
+export class HomeRoutingModule { }
