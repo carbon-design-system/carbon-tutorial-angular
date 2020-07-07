5dc6b2e8758c2291f1981dae2275ad0de20684c1
Palash Roy <palash.singharoy@in.ibm.com>
2020-07-07 14:51:55 +0530

    feat(tutorial): complete step 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,28 +8,19 @@ import { Link1Component } from './pages/link1/link1.component';
 const routes: Routes = [
 	{
 		path: '',
-		loadChildren: () => import('./starter-home/starter-home.module').then(m => m.StarterHomeModule)
+		loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
 	},
 	{
-		path: 'catalog',
-		component: CatalogComponent
+		path: 'repos',
+		loadChildren: () =>
+			import('./repositories/repositories.module').then(
+				(m) => m.RepositoriesModule
+			),
 	},
-	{
-		path: 'docs',
-		component: DocsComponent
-	},
-	{
-		path: 'support',
-		component: SupportComponent
-	},
-	{
-		path: 'link1',
-		component: Link1Component
-	}
 ];
 
 @NgModule({
-	imports: [RouterModule.forRoot(routes)],
-	exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes, { useHash: true })],
+	exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
