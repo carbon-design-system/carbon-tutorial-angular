1d24b9238096851762f138ea5ac35635f6fb03b5
Tim Gerber <gerbs@us.ibm.com>
2020-07-13 12:18:35 -0500

    WIP for Step 2

--- src/app/home/landing-page/landing-page.component.ts
+++ src/app/home/landing-page/landing-page.component.ts
@@ -1,9 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import {
+	BreadcrumbModule,
+	ButtonModule,
+	GridModule,
+	TabsModule,
+} from 'carbon-components-angular';
 @Component({
 	selector: 'app-landing-page',
 	templateUrl: './landing-page.component.html',
 	styleUrls: ['./landing-page.component.scss']
 })
+
+@NgModule({
+	declarations: [LandingPageComponent],
+	imports: [
+		CommonModule,
+		GridModule,
+		BreadcrumbModule,
+		ButtonModule,
+		TabsModule
+	]
+})
+
 export class LandingPageComponent {
 }
