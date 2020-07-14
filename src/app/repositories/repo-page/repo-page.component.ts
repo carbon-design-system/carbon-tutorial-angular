08d56fa20ba9bc4b78aca6db1f96887075443324
jhoose <jhoose@ibm.com>
2020-07-13 21:28:00 -0400

    Step 2 Work In Progress - looks good but doesn't pass npm tests
    Step 2 Work In Progress - looks good but doesn't pass npm tests

--- src/app/repositories/repo-page/repo-page.component.ts
+++ src/app/repositories/repo-page/repo-page.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { RepoTableComponent } from '../repo-table/repo-table.component';
+
 @Component({
 	selector: 'app-repo-page',
 	templateUrl: './repo-page.component.html',
