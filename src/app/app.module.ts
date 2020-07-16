74a2ac10ef7743fa73e8aad78fdf3fcbf00d7247
Jhonatan De Queiroz Silva <jhonatan.de.queiroz.silva@ibm.com>
2020-07-16 13:58:31 -0400

    feat(tutorial): complete step 1

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+// carbon-components-angular default imports
+import { UIShellModule } from 'carbon-components-angular';
+import { Notification20Module } from '@carbon/icons-angular/lib/notification/20';
+import { UserAvatar20Module } from '@carbon/icons-angular/lib/user--avatar/20';
+import { AppSwitcher20Module } from '@carbon/icons-angular/lib/app-switcher/20';
+import { HeaderComponent } from './header/header.component';
+import { CatalogComponent } from './pages/catalog/catalog.component';
+import { DocsComponent } from './pages/docs/docs.component';
+import { SupportComponent } from './pages/support/support.component';
+import { Link1Component } from './pages/link1/link1.component';
+
+@NgModule({
+	declarations: [
+		AppComponent,
+		HeaderComponent,
+		CatalogComponent,
+		DocsComponent,
+		SupportComponent,
+		Link1Component
+	],
+	imports: [
+		BrowserModule,
+		BrowserAnimationsModule,
+		FormsModule,
+		AppRoutingModule,
+		UIShellModule,
+		Notification20Module,
+		UserAvatar20Module,
+		AppSwitcher20Module
+	],
+	bootstrap: [AppComponent]
+})
+export class AppModule { }
