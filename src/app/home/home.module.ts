cedf873a69ca98f91873c35e2b63b49ee86c88a7
Joao Pedro <dp.joaopedro@gmail.com>
2020-07-21 22:09:18 -0300

    feat(tutorial): complete step 4

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -7,9 +7,9 @@ import {
 	BreadcrumbModule,
 	ButtonModule,
 	GridModule,
-	TabsModule
+	TabsModule,
 } from 'carbon-components-angular';
-
+import { InfoModule } from './../info/info.module';
 @NgModule({
 	declarations: [LandingPageComponent],
 	imports: [
@@ -18,7 +18,8 @@ import {
 		BreadcrumbModule,
 		ButtonModule,
 		GridModule,
-		TabsModule
-	]
+		TabsModule,
+		InfoModule,
+	],
 })
-export class HomeModule { }
+export class HomeModule {}
