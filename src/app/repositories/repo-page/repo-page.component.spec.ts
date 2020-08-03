c977f1ebc52f148f7c3f5e012206fb9e83df949a
nethrasms <nevikram@in.ibm.com>
2020-08-03 22:35:18 +0530

    fix the test cases

--- src/app/repositories/repo-page/repo-page.component.spec.ts
+++ src/app/repositories/repo-page/repo-page.component.spec.ts
@@ -3,6 +3,12 @@ import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 import { RepoPageComponent } from './repo-page.component';
 import { GridModule, TableModule } from 'carbon-components-angular';
 import { RepoTableComponent } from '../repo-table/repo-table.component';
+import {
+    ApolloTestingModule,
+    ApolloTestingController,
+} from 'apollo-angular/testing';
+
+import { Apollo } from 'apollo-angular';
 
 describe('RepoPageComponent', () => {
 	let component: RepoPageComponent;
@@ -11,7 +17,9 @@ describe('RepoPageComponent', () => {
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
 			declarations: [ RepoPageComponent, RepoTableComponent ],
+            providers: [ Apollo ],
 			imports: [
+                ApolloTestingModule,
 				GridModule,
 				TableModule
 			],
--- src/app/repositories/repo-table/repo-table.component.spec.ts
+++ src/app/repositories/repo-table/repo-table.component.spec.ts
@@ -2,6 +2,12 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 import { RepoTableComponent } from './repo-table.component';
 import { GridModule, TableModule, PaginationModule, LinkModule } from 'carbon-components-angular';
+import {
+	ApolloTestingModule,
+	ApolloTestingController,
+} from 'apollo-angular/testing';
+
+import { Apollo } from 'apollo-angular';
 
 describe('RepoTableComponent', () => {
 	let component: RepoTableComponent;
@@ -11,12 +17,19 @@ describe('RepoTableComponent', () => {
 		TestBed.configureTestingModule({
 			declarations: [ RepoTableComponent ],
 			imports: [
-				GridModule, TableModule, PaginationModule, LinkModule
+				ApolloTestingModule.withClients(['clientA', 'clientB']),
+								GridModule,
+								TableModule,
+								PaginationModule,
+								LinkModule
 			],
+						providers: [ Apollo ],
 			schemas: [ CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA ]
 		})
 		.compileComponents();
+
 	}));
+
 
 	beforeEach(() => {
 		fixture = TestBed.createComponent(RepoTableComponent);
