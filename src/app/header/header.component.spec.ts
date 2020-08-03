266592b2c79d7cebea16420826cba0d873e5ff2c
Rakesh.K <rkatta10@in.ibm.com>
2020-08-03 15:35:38 +0530

    Fix Step 1

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,9 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
-import { Notification20Module } from '@carbon/icons-angular/lib/notification/20';
-import { UserAvatar20Module } from '@carbon/icons-angular/lib/user--avatar/20';
-import { AppSwitcher20Module } from '@carbon/icons-angular/lib/app-switcher/20';
+import { UIShellModule } from 'carbon-components-angular';
 
 describe('HeaderComponent', () => {
 	let component: HeaderComponent;
@@ -11,14 +9,10 @@ describe('HeaderComponent', () => {
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
-			declarations: [
-				HeaderComponent,
-				Notification20Module,
-				UserAvatar20Module,
-				AppSwitcher20Module
-			]
+			declarations: [HeaderComponent],
+			imports: [UIShellModule]
 		})
-		.compileComponents();
+			.compileComponents();
 	}));
 
 	beforeEach(() => {
