f41c03b25ba985bca22ae417aa0881745928e617
Tim Gerber <gerbs@us.ibm.com>
2020-08-07 14:55:26 -0500

    feat(tutorial): complete step 3 (fix testing)

--- src/app/repositories/repo-table/repo-table.component.spec.ts
+++ src/app/repositories/repo-table/repo-table.component.spec.ts
@@ -1,7 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+// import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 import { RepoTableComponent } from './repo-table.component';
-import { TableModule, LinkModule, PaginationModule } from 'carbon-components-angular';
+import { GridModule, TableModule, PaginationModule, LinkModule } from 'carbon-components-angular';
+import {
+	ApolloTestingModule,
+	ApolloTestingController,
+} from 'apollo-angular/testing';
+
+import { Apollo } from 'apollo-angular';
 
 describe('RepoTableComponent', () => {
 	let component: RepoTableComponent;
@@ -9,15 +15,21 @@ describe('RepoTableComponent', () => {
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
-			declarations: [ RepoTableComponent ],
+			declarations: [RepoTableComponent],
 			imports: [
+				ApolloTestingModule.withClients(['clientA', 'clientB']),
+				GridModule,
 				TableModule,
-				LinkModule,
-				PaginationModule
-			]
+				PaginationModule,
+				LinkModule
+			],
+			providers: [Apollo],
+			// schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
 		})
-		.compileComponents();
+			.compileComponents();
+
 	}));
+
 
 	beforeEach(() => {
 		fixture = TestBed.createComponent(RepoTableComponent);
