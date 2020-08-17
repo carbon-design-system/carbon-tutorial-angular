c46d7999d335e7ada769ef84bac55c288f4a973e
vishast1 <vishast1@in.ibm.com>
2020-08-17 17:55:14 +0530

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 
-
 // carbon-components-angular default imports
 import { UIShellModule } from 'carbon-components-angular';
 
