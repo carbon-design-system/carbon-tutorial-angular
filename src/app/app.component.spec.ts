bf542f87e58f4354b4d15f951393de632f1197d4
vishast1 <vishast1@in.ibm.com>
2020-08-17 16:13:21 +0530

    feat(tutorial): complete step 1

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,34 @@
+import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+import { UIShellModule } from 'carbon-components-angular';
+import { NotificationModule } from '@carbon/icons-angular/';
+import { UserAvatarModule } from '@carbon/icons-angular/';
+import { AppSwitcherModule } from '@carbon/icons-angular/';
+import { HeaderComponent } from './header/header.component';
+
+describe('AppComponent', () => {
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			imports: [
+				RouterTestingModule,
+				UIShellModule,
+				NotificationModule,
+				UserAvatarModule,
+				AppSwitcherModule
+			],
+			declarations: [
+				AppComponent,
+				HeaderComponent
+			],
+		}).compileComponents();
+	}));
+
+	it('should create the app', () => {
+		const fixture = TestBed.createComponent(AppComponent);
+		const app = fixture.componentInstance;
+		expect(app).toBeTruthy();
+	});
+});
+
+
---
+++ src/app/header/header.component.spec.ts
@@ -0,0 +1,30 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HeaderComponent } from './header.component';
+import { UIShellModule } from 'carbon-components-angular/ui-shell/';
+import { NotificationModule } from '@carbon/icons-angular/';
+import { UserAvatarModule } from '@carbon/icons-angular/';
+import { AppSwitcherModule } from '@carbon/icons-angular/';
+
+describe('HeaderComponent', () => {
+	let component: HeaderComponent;
+	let fixture: ComponentFixture<HeaderComponent>;
+
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [ HeaderComponent ],
+			imports: [UIShellModule, NotificationModule, UserAvatarModule, AppSwitcherModule]
+		})
+		.compileComponents();
+	}));
+
+	beforeEach(() => {
+		fixture = TestBed.createComponent(HeaderComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
+
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+});
