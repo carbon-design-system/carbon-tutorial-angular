e5b99500985e0a1fb56f2eeed8176ec93f4d4c71
Bhushan Fegade <bfegade@MacBook-Pro.local>
2020-08-18 17:36:49 -0400

    feat(tutorial): complete step 1

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { StarterHomeComponent } from './starter-home/starter-home.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
-const routes: Routes = [{
+
+const routes: Routes = [ {
 	path: '',
-	component: StarterHomeComponent
-}];
+	component: LandingPageComponent,
+	},
+];
 
 @NgModule({
 	imports: [RouterModule.forChild(routes)],
 	exports: [RouterModule]
 })
-export class StarterHomeRoutingModule { }
+export class HomeRoutingModule { }
