2798819ee32e1526b1a018611a0851f3b941edb1
Dave Foley <dfoley@salesforce.com>
2020-08-23 02:16:13 -0400

    feat(tutorial): complete step 3

--- src/app/repositories/repo-table/repo-table.component.spec.ts
+++ src/app/repositories/repo-table/repo-table.component.spec.ts
@@ -1,20 +1,27 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { ApolloTestingModule } from "apollo-angular/testing";
 
-import { RepoTableComponent } from './repo-table.component';
-import { TableModule } from 'carbon-components-angular';
+import { RepoTableComponent } from "./repo-table.component";
+import {
+	TableModule,
+	LinkModule,
+	PaginationModule,
+} from "carbon-components-angular";
 
-describe('RepoTableComponent', () => {
+describe("RepoTableComponent", () => {
 	let component: RepoTableComponent;
 	let fixture: ComponentFixture<RepoTableComponent>;
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
-			declarations: [ RepoTableComponent ],
+			declarations: [RepoTableComponent],
 			imports: [
-				TableModule
-			]
-		})
-		.compileComponents();
+				TableModule,
+				LinkModule,
+				PaginationModule,
+				ApolloTestingModule,
+			],
+		}).compileComponents();
 	}));
 
 	beforeEach(() => {
@@ -23,7 +30,7 @@ describe('RepoTableComponent', () => {
 		fixture.detectChanges();
 	});
 
-	it('should create', () => {
+	it("should create", () => {
 		expect(component).toBeTruthy();
 	});
 });
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,7 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-	production: false
-};
+			production: false,
+			githubPersonalAccessToken:
+				"ce6c61c7d13cb93d1601e1c4deefb0b9f204c968",
+		};
