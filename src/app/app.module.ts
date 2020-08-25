7335e91bb2f831c7e1039802840c94c7bc9a7a4a
yingxuan <yingxuanchen@hotmail.com>
2020-08-25 17:32:00 +0800

    feat(tutorial): complete step 1

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { NotificationModule } from '@carbon/icons-angular';
+import { UserAvatarModule } from '@carbon/icons-angular';
+import { AppSwitcherModule } from '@carbon/icons-angular';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+
+// carbon-components-angular default imports
+import { UIShellModule } from 'carbon-components-angular';
+
+@NgModule({
+	declarations: [
+		AppComponent,
+		HeaderComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		UIShellModule,
+		NotificationModule,
+		UserAvatarModule,
+		AppSwitcherModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
+})
+export class AppModule { }
