22a737d589469cb3dd82088dbc6f933597a5df7c
PriyaTawate <pritawat@in.ibm.com>
2020-08-26 11:51:17 +0530

    feat(tutorial) step 1 completed

--- src/app/repositories/repositories-routing.module.ts
+++ src/app/repositories/repositories-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { StarterHomeComponent } from './starter-home/starter-home.component';
+import { RepoPageComponent } from './repo-page/repo-page.component';
 
-const routes: Routes = [{
+const routes: Routes = [
+{
 	path: '',
-	component: StarterHomeComponent
-}];
+	component: RepoPageComponent
+}
+];
 
 @NgModule({
 	imports: [RouterModule.forChild(routes)],
 	exports: [RouterModule]
 })
-export class StarterHomeRoutingModule { }
+export class RepositoriesRoutingModule { }
