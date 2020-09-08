2ea51c83338d7ee8b2bddbecfd98cd3b4c6e5cc5
lanzejun <lanzejun@cn.ibm.com>
2020-09-08 22:42:56 +0800

    feat(tutorial): complete step 1

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 
-
 const routes: Routes = [
 	{
 		path: '',
-		component: LandingPageComponent,
-	},
+		component: LandingPageComponent
+	}
 ];
 
 @NgModule({
