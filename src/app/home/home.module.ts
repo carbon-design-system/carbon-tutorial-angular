e1f75612fb5956ccab2fcf7bae94012fab9a732c
Aakansha Agrawal <aakaagra@in.ibm.com>
2020-09-08 14:11:06 +0530

    feat(tutorial): complete step 2

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { LandingPageComponent } from './landing-page/landing-page.component';
-
 import {
 	BreadcrumbModule,
 	ButtonModule,
