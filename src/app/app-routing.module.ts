5b4f2dc9dd2ce557b7f08dcb4ad6668feb837b71
ivanbogar <ivanbogar@gmail.com>
2020-09-10 15:56:51 -0300

    feat(tutorial): complete step 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { CatalogComponent } from './pages/catalog/catalog.component';
-import { DocsComponent } from './pages/docs/docs.component';
-import { SupportComponent } from './pages/support/support.component';
-import { Link1Component } from './pages/link1/link1.component';
+// import { CatalogComponent } from './pages/catalog/catalog.component';
+// import { DocsComponent } from './pages/docs/docs.component';
+// import { SupportComponent } from './pages/support/support.component';
+// import { Link1Component } from './pages/link1/link1.component';
 
 const routes: Routes = [
 {
