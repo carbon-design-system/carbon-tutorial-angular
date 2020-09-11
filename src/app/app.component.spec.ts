18dcd2e5f5b35bc2bfee06dcd9213dc0b6ce9314
Jerbee Macalino <Jerbee.L.Macalino@ibm.com>
2020-09-11 10:50:25 +0800

    feat(tutorial): complete step 1

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -35,11 +35,4 @@ describe('AppComponent', () => {
 		const app = fixture.debugElement.componentInstance;
 		expect(app.title).toEqual('carbon-angular-tutorial');
 	});
-
-	it('should render title', () => {
-		const fixture = TestBed.createComponent(AppComponent);
-		fixture.detectChanges();
-		const compiled = fixture.debugElement.nativeElement;
-		expect(compiled.querySelector('.content span').textContent).toContain('carbon-angular-tutorial app is running!');
-	});
 });
