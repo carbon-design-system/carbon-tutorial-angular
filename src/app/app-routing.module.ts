d6825b07cbc66f3652ebc11a200253d87ff96e85
Shivendra Chauhan <shivendra_chauhan@outlook.com>
2020-09-14 01:33:28 +0530

    feat(tutorial): complete step 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { CatalogComponent } from './pages/catalog/catalog.component';
-import { DocsComponent } from './pages/docs/docs.component';
-import { SupportComponent } from './pages/support/support.component';
-import { Link1Component } from './pages/link1/link1.component';
 
 const routes: Routes = [
 	{
@@ -17,22 +13,6 @@ const routes: Routes = [
 			(m) => m.RepositoriesModule
 			),
 	},
-	{
-		path: 'catalog',
-		component: CatalogComponent
-	},
-	{
-		path: 'docs',
-		component: DocsComponent
-	},
-	{
-		path: 'support',
-		component: SupportComponent
-	},
-	{
-		path: 'link1',
-		component: Link1Component
-	}
 ];
 
 @NgModule({
