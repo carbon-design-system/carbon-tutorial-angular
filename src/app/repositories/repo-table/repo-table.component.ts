83c19d672f1b3afb9e8e61902997e5b13e65c9a9
Louis Riel <louis-sebastien.riel@ca.ibm.com>
2020-09-17 16:12:10 -0400

    test1

--- src/app/repositories/repo-table/repo-table.component.ts
+++ src/app/repositories/repo-table/repo-table.component.ts
@@ -18,64 +18,18 @@ import gql from 'graphql-tag';
 
 
 export class RepoTableComponent implements OnInit {
-	model = new TableModel();
-	@ViewChild('linkTemplate', null)
-	protected linkTemplate: TemplateRef<any>;
+	data = [];
+	model: TableModel;
 	skeletonModel = Table.skeletonModel(10, 6);
 	skeleton = true;
 
+	@ViewChild('linkTemplate', null)
+	protected linkTemplate: TemplateRef<any>;
+
 	constructor(private apollo: Apollo) { }
 
-	prepareData(data) {
-		const newData = [];
-		this.skeleton = false;
-
-		for (const datum of data) {
-			newData.push([
-			new TableItem({ data: datum.name, expandedData: datum.description }),
-			new TableItem({ data: new Date(datum.createdAt).toLocaleDateString() }),
-			new TableItem({ data: new Date(datum.updatedAt).toLocaleDateString() }),
-			new TableItem({ data: datum.issues.totalCount }),
-			new TableItem({ data: datum.stargazers.totalCount }),
-			new TableItem({
-				data: {
-				github: datum.url,
-				homepage: datum.homepageUrl
-				},
-				template: this.linkTemplate
-			})
-			]);
-		}
-		return newData;
-	}
-
 	ngOnInit() {
-		this.model.data = [
-			[
-				new TableItem({data: 'Repo 1', expandedData: 'Row description'}),
-				new TableItem({data: 'Date'}),
-				new TableItem({data: 'Date'}),
-				new TableItem({data: '123'}),
-				new TableItem({data: '456'}),
-				new TableItem({data: 'Links'})
-			],
-			[
-				new TableItem({data: 'Repo 2', expandedData: 'Row description'}),
-				new TableItem({data: 'Date'}),
-				new TableItem({data: 'Date'}),
-				new TableItem({data: '123'}),
-				new TableItem({data: '456'}),
-				new TableItem({data: 'Links'})
-			],
-			[
-				new TableItem({data: 'Repo 3', expandedData: 'Row description'}),
-				new TableItem({data: 'Date'}),
-				new TableItem({data: 'Date'}),
-				new TableItem({data: '123'}),
-				new TableItem({data: '456'}),
-				new TableItem({data: 'Links'})
-			]
-		];
+		this.model = new TableModel();
 		this.model.header = [
 			new TableHeaderItem({data: 'Name'}),
 			new TableHeaderItem({data: 'Created'}),
@@ -84,53 +38,87 @@ export class RepoTableComponent implements OnInit {
 			new TableHeaderItem({data: 'Stars'}),
 			new TableHeaderItem({data: 'Links'})
 		];
+		this.model.pageLength = 10;
+
 		this.apollo.watchQuery({
 			query: gql`
-				query REPO_QUERY {
+			  query REPO_QUERY {
 				# Let's use carbon as our organization
 				organization(login: "carbon-design-system") {
-					# We'll grab all the repositories in one go. To load more resources
-					# continuously, see the advanced topics.
-					repositories(first: 75, orderBy: { field: UPDATED_AT, direction: DESC }) {
+				  # We'll grab all the repositories in one go. To load more resources
+				  # continuously, see the advanced topics.
+				  repositories(first: 75, orderBy: { field: UPDATED_AT, direction: DESC }) {
 					totalCount
 					nodes {
-						url
-						homepageUrl
-						issues(filterBy: { states: OPEN }) {
+					  url
+					  homepageUrl
+					  issues(filterBy: { states: OPEN }) {
 						totalCount
-						}
-						stargazers {
+					  }
+					  stargazers {
 						totalCount
-						}
-						releases(first: 1) {
+					  }
+					  releases(first: 1) {
 						totalCount
 						nodes {
-							name
+						  name
 						}
-						}
-						name
-						updatedAt
-						createdAt
-						description
-						id
+					  }
+					  name
+					  updatedAt
+					  createdAt
+					  description
+					  id
 					}
-					}
+				  }
 				}
-			}
+			  }
 			`
 		}).valueChanges.subscribe((response: any) => {
 			if (response.error) {
 				const errorData = [];
-				errorData.push([
+			 	errorData.push([
 					new TableItem({data: 'error!' })
 				]);
 				this.model.data = errorData;
 			} else if (response.loading) {
-					this.skeleton = true;
+				this.skeleton = true;
 			} else {
 				// If we're here, we've got our data!
-				this.prepareData(response.data.organization.repositories.nodes);
+				this.data = response.data.organization.repositories.nodes;
+				this.model.totalDataLength = response.data.organization.repositories.totalCount;
+				this.selectPage(1);
 			}
 		});
+	}
+
+	selectPage(page) {
+		const offset = this.model.pageLength * (page - 1);
+		const pageRawData = this.data.slice(offset, offset + this.model.pageLength);
+		this.model.data = this.prepareData(pageRawData);
+		this.model.currentPage = page;
+	}
+
+	prepareData(data) {
+		this.skeleton = false;
+		const newData = [];
+
+		for (const datum of data) {
+			newData.push([
+				new TableItem({ data: datum.name, expandedData: datum.description }),
+				new TableItem({ data: new Date(datum.createdAt).toLocaleDateString() }),
+				new TableItem({ data: new Date(datum.updatedAt).toLocaleDateString() }),
+				new TableItem({ data: datum.issues.totalCount }),
+				new TableItem({ data: datum.stargazers.totalCount }),
+				new TableItem({
+					data: {
+						github: datum.url,
+						homepage: datum.homepageUrl
+					},
+					template: this.linkTemplate
+				})
+			]);
+		}
+		return newData;
 	}
 }
