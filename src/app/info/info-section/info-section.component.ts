1855826b4f7e9d879a3e9e24f70cec3422d9e074
ivanbogar <ivanbogar@gmail.com>
2020-09-17 23:20:39 -0300

    feat(tutorial): complete step 4

---
+++ src/app/info/info-section/info-section.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import * as data from '../info.json';
+
+@Component({
+	selector: 'app-info-section',
+	templateUrl: './info-section.component.html',
+	styleUrls: ['./info-section.component.scss']
+})
+export class InfoSectionComponent {
+
+	heading = data.title;
+	items = data.items;
+
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,5 @@
 
 export const environment = {
 	production: false,
-	githubPersonalAccessToken: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+	githubPersonalAccessToken: '5e38fa0bb14cd3c33d4d96a7fb905881463d3fc9'
 };
