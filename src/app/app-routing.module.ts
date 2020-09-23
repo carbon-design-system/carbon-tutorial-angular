194ff59925115441c3fa6d84c787f6bfbd3b9847
Merhan Elkheshen <merhanelkheshen@gmail.com>
2020-09-23 11:45:07 +0200

    feat(tutorial): complete step 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { CatalogComponent } from './pages/catalog/catalog.component';
-import { DocsComponent } from './pages/docs/docs.component';
-import { SupportComponent } from './pages/support/support.component';
-import { Link1Component } from './pages/link1/link1.component';
 
 const routes: Routes = [
 	{
@@ -16,25 +12,8 @@ const routes: Routes = [
 			import('./repositories/repositories.module').then(
 				(m) => m.RepositoriesModule
 			),
-	},
-	{
-		path: 'catalog',
-		component: CatalogComponent
-	},
-	{
-		path: 'docs',
-		component: DocsComponent
-	},
-	{
-		path: 'support',
-		component: SupportComponent
-	},
-	{
-		path: 'link1',
-		component: Link1Component
 	}
 ];
-
 @NgModule({
 	imports: [RouterModule.forRoot(routes, { useHash: true })],
 	exports: [RouterModule]
