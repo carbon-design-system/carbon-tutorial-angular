ba47cb185ce79b5084cc2e7d44800fab64893275
Xianzhan Lin <xlin@Xianzhans-MacBook-Pro.local>
2020-10-04 21:47:07 -0700

    added landing page

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { GridModule } from 'carbon-components-angular';
+import { BreadcrumbModule, ButtonModule, TabsModule } from 'carbon-components-angular';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { LandingPageComponent } from './landing-page/landing-page.component';
@@ -7,8 +8,12 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 @NgModule({
 	declarations: [LandingPageComponent],
 	imports: [
-		CommonModule,
-		HomeRoutingModule
+    //	CommonModule,
+		HomeRoutingModule,
+        GridModule,
+        BreadcrumbModule,
+        ButtonModule,
+        TabsModule
 	]
 })
 export class HomeModule { }
--- src/app/home/landing-page/landing-page.component.spec.ts
+++ src/app/home/landing-page/landing-page.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { GridModule } from 'carbon-components-angular';
 
 import { LandingPageComponent } from './landing-page.component';
+import { BreadcrumbModule, GridModule, ButtonModule, TabsModule } from 'carbon-components-angular';
 
 describe('LandingPageComponent', () => {
 	let component: LandingPageComponent;
@@ -8,7 +10,8 @@ describe('LandingPageComponent', () => {
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
-			declarations: [ LandingPageComponent ]
+			declarations: [ LandingPageComponent ],
+            imports: [BreadcrumbModule, GridModule]
 		})
 		.compileComponents();
 	}));
