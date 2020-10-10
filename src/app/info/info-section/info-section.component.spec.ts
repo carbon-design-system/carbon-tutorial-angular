c1b1e2277b92be5f8fc6c337f697a66af222df63
Jose-Ruben-Suarez <jose.ruben.suarez@ibm.com>
2020-10-10 13:41:19 -0500

    feat(tutorial): complete step 4

---
+++ src/app/info/info-section/info-section.component.spec.ts
@@ -0,0 +1,38 @@
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { GridModule } from "carbon-components-angular";
+
+import { InfoSectionComponent } from "./info-section.component";
+
+import { PersonFavorite32Module } from "@carbon/icons-angular/lib/person--favorite/32";
+import { Globe32Module } from "@carbon/icons-angular/lib/globe/32";
+import { Application32Module } from "@carbon/icons-angular/lib/application/32";
+import { InfoCardComponent } from '../info-card/info-card.component';
+
+
+
+describe("InfoSectionComponent", () => {
+	let component: InfoSectionComponent;
+	let fixture: ComponentFixture<InfoSectionComponent>;
+
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [InfoSectionComponent, InfoCardComponent],
+			imports: [
+				GridModule,
+				PersonFavorite32Module,
+				Globe32Module,
+				Application32Module
+			]
+		}).compileComponents();
+	}));
+
+	beforeEach(() => {
+		fixture = TestBed.createComponent(InfoSectionComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
+
+	it("should create", () => {
+		expect(component).toBeTruthy();
+	});
+});
