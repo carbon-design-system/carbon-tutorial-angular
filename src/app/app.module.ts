2929310863a53087d3914959808ca26c57fc3688
Christian Templin <christian.templin@de.ibm.com>
2020-10-12 16:43:10 +0200

    feat(tutorial): complete step 1

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+
+// carbon-components-angular default imports
+import { UIShellModule } from 'carbon-components-angular';
+
+import { NotificationModule, UserAvatarModule, AppSwitcherModule } from '@carbon/icons-angular';
+
+@NgModule({
+	declarations: [AppComponent, HeaderComponent],
+	imports: [BrowserModule, AppRoutingModule, UIShellModule, NotificationModule, UserAvatarModule, AppSwitcherModule],
+	bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component, HostBinding, OnInit } from '@angular/core';
+@Component({
+	selector: 'app-header',
+	templateUrl: './header.component.html',
+	styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+	@HostBinding('class.bx--header') headerClass = true;
+	constructor() {}
+
+	ngOnInit(): void {}
+}
