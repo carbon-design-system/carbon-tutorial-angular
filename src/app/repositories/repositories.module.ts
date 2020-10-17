f0fc2014d3d3fd715dcfd1208ea22308f90f58a7
Navil <rodriguesnavil@gmail.com>
2020-10-17 20:31:35 +0530

    feat(tutorial): complete step 3(Updated)

--- src/app/repositories/repositories.module.ts
+++ src/app/repositories/repositories.module.ts
@@ -3,7 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { RepositoriesRoutingModule } from './repositories-routing.module';
 import { RepoPageComponent } from './repo-page/repo-page.component';
-import { GridModule, TableModule } from 'carbon-components-angular';
+import { GridModule,
+	TableModule,
+	LinkModule,
+	PaginationModule
+} from 'carbon-components-angular';
 import { RepoTableComponent } from './repo-table/repo-table.component';
 
 @NgModule({
@@ -12,7 +16,9 @@ import { RepoTableComponent } from './repo-table/repo-table.component';
 		CommonModule,
 		RepositoriesRoutingModule,
 		GridModule,
- 		TableModule
+		TableModule,
+		LinkModule,
+		PaginationModule
 	]
 })
 export class RepositoriesModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,6 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-	production: false
+	production: false,
+	githubPersonalAccessToken: 'acb726bfe99accf24dd465db5fceab703ecfee3b'
 };
