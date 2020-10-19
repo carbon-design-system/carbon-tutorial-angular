ccf2c11716fc65be2eef7ff0ba43545ed1efaea6
pooshonbanerjee <40276600+pooshonbanerjee@users.noreply.github.com>
2020-10-19 23:10:30 +0530

    Update landing-page.component.spec.ts

--- src/app/home/landing-page/landing-page.component.spec.ts
+++ src/app/home/landing-page/landing-page.component.spec.ts
@@ -1,6 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LandingPageComponent } from './landing-page.component';
+import { InfoSectionComponent } from './../../info/info-section/info-section.component';
+import { InfoCardComponent } from './../../info/info-card/info-card.component';
+import { PersonFavorite32Module } from "@carbon/icons-angular/lib/person--favorite/32";
+import { Globe32Module } from "@carbon/icons-angular/lib/globe/32";
+import { Application32Module } from "@carbon/icons-angular/lib/application/32";
+
 import {
 	BreadcrumbModule,
 	ButtonModule,
@@ -14,12 +20,15 @@ describe('LandingPageComponent', () => {
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
-			declarations: [ LandingPageComponent ],
+			declarations: [ LandingPageComponent, InfoSectionComponent, InfoCardComponent ],
 			imports: [
 				BreadcrumbModule,
 				ButtonModule,
 				GridModule,
-				TabsModule
+				TabsModule,
+				PersonFavorite32Module,
+				Globe32Module,
+				Application32Module
 			]
 		})
 		.compileComponents();
