455c1eee2d53da467a992cb67904383204057366
pooshonbanerjee <40276600+pooshonbanerjee@users.noreply.github.com>
2020-10-19 23:09:25 +0530

    Update info-section.component.spec.ts

--- src/app/info/info-section/info-section.component.spec.ts
+++ src/app/info/info-section/info-section.component.spec.ts
@@ -1,14 +1,25 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { InfoSectionComponent } from './info-section.component';
-
+import { InfoCardComponent } from './../info-card/info-card.component';
+import { GridModule } from "carbon-components-angular";
+import { PersonFavorite32Module } from "@carbon/icons-angular/lib/person--favorite/32";
+import { Globe32Module } from "@carbon/icons-angular/lib/globe/32";
+import { Application32Module } from "@carbon/icons-angular/lib/application/32";
+import * as data from '../info.json';
 describe('InfoSectionComponent', () => {
 	let component: InfoSectionComponent;
 	let fixture: ComponentFixture<InfoSectionComponent>;
+	// let data:any = dataJSON;
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
-			declarations: [ InfoSectionComponent ]
+			declarations: [ InfoSectionComponent, InfoCardComponent ],
+			imports: [
+				GridModule,
+				PersonFavorite32Module,
+				Globe32Module,
+				Application32Module
+			]
 		})
 		.compileComponents();
 	}));
@@ -16,6 +27,9 @@ describe('InfoSectionComponent', () => {
 	beforeEach(() => {
 		fixture = TestBed.createComponent(InfoSectionComponent);
 		component = fixture.componentInstance;
+		component.heading = data.title;
+	  component.items = data.items;
+
 		fixture.detectChanges();
 	});
 
