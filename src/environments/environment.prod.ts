ce3a088e9031f588d75b9c1cd94b0088462a4196
Pooshon Banerjee <pooshonbanerjee@in.ibm.com>
2020-10-19 14:06:35 +0530

    feat(tutorial): complete step 5

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
 	production: true,
-	githubPersonalAccessToken: '2d1d6f1914d46d3570b7ce8b2c7d7d12939f1a7f'
+	githubPersonalAccessToken: 'ba6b373d156096e147077f3fe85a6939d6754042'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,5 @@
 
 export const environment = {
 	production: false,
-	githubPersonalAccessToken: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+	githubPersonalAccessToken: 'ba6b373d156096e147077f3fe85a6939d6754042'
 };
