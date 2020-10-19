adb66174ab8ee48f409115f6bb759a0a6c691ecc
Pooshon Banerjee <pooshonbanerjee@in.ibm.com>
2020-10-19 13:13:58 +0530

    feat(tutorial): complete step 4

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { InfoModule } from "./../info/info.module";
 import { HomeRoutingModule } from './home-routing.module';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { InfoSectionComponent } from './../info/info-section/info-section.component';
+import { InfoCardComponent } from './../info/info-card/info-card.component';
+import { PersonFavorite32Module } from "@carbon/icons-angular/lib/person--favorite/32";
+import { Globe32Module } from "@carbon/icons-angular/lib/globe/32";
+import { Application32Module } from "@carbon/icons-angular/lib/application/32";
+
 import {
 	BreadcrumbModule,
 	ButtonModule,
@@ -11,14 +17,17 @@ import {
 } from 'carbon-components-angular';
 
 @NgModule({
-	declarations: [LandingPageComponent],
+	declarations: [LandingPageComponent, InfoSectionComponent, InfoCardComponent],
 	imports: [
 		CommonModule,
 		HomeRoutingModule,
 		BreadcrumbModule,
 		ButtonModule,
 		GridModule,
-		TabsModule
+		TabsModule,
+		PersonFavorite32Module,
+		Globe32Module,
+		Application32Module
 	]
 })
 export class HomeModule { }
