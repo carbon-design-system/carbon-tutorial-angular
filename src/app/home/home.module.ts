06ce5caca02fb751d0d1473b8c2acbfe365d47e0
Oleg Kashaev <oleg.kashaev@pl.ibm.com>
2020-10-29 08:36:32 +0100

    save work

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BreadcrumbModule, GridModule } from 'carbon-components-angular';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+
 
 @NgModule({
 	declarations: [LandingPageComponent],
 	imports: [
 		CommonModule,
-		HomeRoutingModule
+		HomeRoutingModule,
+		BreadcrumbModule,
+		GridModule
 	]
 })
 export class HomeModule { }
--- src/app/home/landing-page/landing-page.component.spec.ts
+++ src/app/home/landing-page/landing-page.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { BreadcrumbModule, GridModule } from 'carbon-components-angular';
 
 import { LandingPageComponent } from './landing-page.component';
 
@@ -8,7 +9,7 @@ describe('LandingPageComponent', () => {
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
-			declarations: [ LandingPageComponent ]
+			declarations: [ LandingPageComponent, BreadcrumbModule, GridModule ]
 		})
 		.compileComponents();
 	}));
