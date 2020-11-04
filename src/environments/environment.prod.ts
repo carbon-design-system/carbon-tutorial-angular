08e9d015f9eb6330d313b75dcf99b114175bccc0
Oleg Kashaev <oleg.kashaev@pl.ibm.com>
2020-11-04 14:40:52 +0100

    feat(tutorial): complete step 4

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
 	production: true,
-	githubPersonalAccessToken: '2d1d6f1914d46d3570b7ce8b2c7d7d12939f1a7f'
+	githubPersonalAccessToken: '966e3b568c629a0ffe3d82f7a5f444e108d0b0f1'
 };
