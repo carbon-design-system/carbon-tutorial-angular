85a60460f899652b05803b26f99da97cde33cc67
pratgm29@in.ibm.com <pratgm29@in.ibm.com>
2020-11-05 14:29:38 +0530

    feat(tutorial): complete step 1

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
-import { UIShellModule } from 'carbon-components-angular';
+import { UIShellModule } from 'carbon-components-angular/ui-shell/ui-shell.module';
 import { Notification20Module } from '@carbon/icons-angular/lib/notification/20';
 import { UserAvatar20Module } from '@carbon/icons-angular/lib/user--avatar/20';
 import { AppSwitcher20Module } from '@carbon/icons-angular/lib/app-switcher/20';
