08c8371cd982c827377901d00642fc3c6a7bfc1a
Indulakshmi K R <indukr01@in.ibm.com>
2020-11-12 21:37:03 +0530

    feat(tutorial): complete step 1

--- karma.conf.js
+++ karma.conf.js
@@ -2,6 +2,7 @@
 // https://karma-runner.github.io/0.13/config/configuration-file.html
 
 module.exports = function (config) {
+	
 	config.set({
 		basePath: '',
 		frameworks: ['jasmine', '@angular-devkit/build-angular'],
