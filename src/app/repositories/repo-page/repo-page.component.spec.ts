edc630b954028324e0c921cd1848e571b57693b1
rkunjappa <reeth.kunjappa@gmail.com>
2020-11-19 09:41:40 +0530

    feat(tutorial): complete step 1

--- src/app/repositories/repo-page/repo-page.component.spec.ts
+++ src/app/repositories/repo-page/repo-page.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RepoPageComponent } from './repo-page.component';
+import { GridModule, TableModule, PaginationModule } from 'carbon-components-angular';
 
 describe('RepoPageComponent', () => {
 	let component: RepoPageComponent;
@@ -8,7 +9,12 @@ describe('RepoPageComponent', () => {
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
-			declarations: [ RepoPageComponent ]
+			declarations: [ RepoPageComponent],
+			imports: [
+				GridModule,
+				TableModule,
+				PaginationModule
+			]
 		})
 		.compileComponents();
 	}));
