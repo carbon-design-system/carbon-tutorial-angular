05684b30e175b007caccd6213018e5f31fa08c13
Sarah Seif <sarahseif@Sarahs-MBP.mshome.net>
2020-11-28 13:12:35 +0200

    feat(tutorial): complete step 5

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
 	production: true,
-	githubPersonalAccessToken: '2d1d6f1914d46d3570b7ce8b2c7d7d12939f1a7f'
+	githubPersonalAccessToken: 'cd3bc81686854489f876dc6052c382c9d739a766'
 };
