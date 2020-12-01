ad8151e66415a11e7f70e7bc8eb342ca5a453ce0
Daniel Moreto <danifim@br.ibm.com>
2020-12-01 16:54:46 -0300

    feat(tutorial): complete step 2

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import {
+	BreadcrumbModule,
+	ButtonModule,
+	GridModule,
+	TabsModule
+} from 'carbon-components-angular';
+
 import { HomeRoutingModule } from './home-routing.module';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+
 
 @NgModule({
 	declarations: [LandingPageComponent],
 	imports: [
 		CommonModule,
-		HomeRoutingModule
+		HomeRoutingModule,
+		BreadcrumbModule,
+		ButtonModule,
+		GridModule,
+		TabsModule
 	]
 })
 export class HomeModule { }
