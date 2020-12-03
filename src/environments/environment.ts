4346c73fa3f228af515ab4f351b53b298cc4b6c9
Indulakshmi K R <indukr01@in.ibm.com>
2020-12-03 08:33:37 +0530

    feat(tutorial): complete step 3

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,10 @@
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
+// githubPersonalAccessToken : 'ec8ada3667f5614756d62ef68f6bc214e2cbac7b'
+
 
 export const environment = {
-	production: false
+	production: false,
+	githubPersonalAccessToken: 'ec8ada3667f5614756d62ef68f6bc214e2cbac7b'
 };
