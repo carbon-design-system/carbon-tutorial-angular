e83e529cb6efbc73395063a3443fea8a96ba526c
GERSON AMADO OJEDA GONZALEZ <GERSON.OJEDA@ibm.com>
2020-12-07 23:22:09 -0600

    feat(tutorial): complete step 4

--- src/app/home/landing-page/landing-page.component.spec.ts
+++ src/app/home/landing-page/landing-page.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LandingPageComponent } from './landing-page.component';
+import { InfoCardComponent } from '../../info/info-card/info-card.component';
+import { InfoSectionComponent } from '../../info/info-section/info-section.component';
 import {
 	BreadcrumbModule,
 	ButtonModule,
@@ -8,18 +10,29 @@ import {
 	TabsModule
 } from 'carbon-components-angular';
 
+import { PersonFavorite32Module } from '@carbon/icons-angular/lib/person--favorite/32';
+import { Globe32Module } from '@carbon/icons-angular/lib/globe/32';
+import { Application32Module } from '@carbon/icons-angular/lib/application/32';
+
 describe('LandingPageComponent', () => {
 	let component: LandingPageComponent;
 	let fixture: ComponentFixture<LandingPageComponent>;
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
-			declarations: [ LandingPageComponent ],
+			declarations: [
+				LandingPageComponent,
+				InfoCardComponent,
+				InfoSectionComponent
+			],
 			imports: [
 				BreadcrumbModule,
 				ButtonModule,
 				GridModule,
-				TabsModule
+				TabsModule,
+				PersonFavorite32Module,
+				Globe32Module,
+				Application32Module
 			]
 		})
 		.compileComponents();
