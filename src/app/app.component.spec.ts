217bb1f94b001e22072460200456ba8ea5c39755
Karolina Kidon Kowalska <karkiko@li-6240a84c-28aa-11b2-a85c-c174a29a310b.ibm.com>
2020-12-28 15:43:30 +0100

    feat(tutorial): complete step 1

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,33 @@
+import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+import { UIShellModule } from 'carbon-components-angular';
+import { HeaderComponent } from './header/header.component';
+import { NotificationModule } from '@carbon/icons-angular';
+import { UserAvatarModule } from '@carbon/icons-angular';
+import { AppSwitcherModule } from '@carbon/icons-angular';
+
+describe('AppComponent', () => {
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			imports: [
+				RouterTestingModule,
+				UIShellModule,
+				NotificationModule,
+				UserAvatarModule,
+				AppSwitcherModule
+			],
+			declarations: [
+				AppComponent,
+				HeaderComponent
+			],
+		}).compileComponents();
+	}));
+
+	it('should create the app', () => {
+		const fixture = TestBed.createComponent(AppComponent);
+		const app = fixture.debugElement.componentInstance;
+		expect(app).toBeTruthy();
+	});
+
+});
