ba6dbd9a1124232509c368d176d2e7e355996b2f
kuroyuki <yuriizhukov@gmail.com>
2020-12-29 14:16:54 +0300

    Update home-routing.module.ts

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+	    path: '',
+	    component: LandingPageComponent,
+  	},
+];
 
 @NgModule({
 	imports: [RouterModule.forChild(routes)],
