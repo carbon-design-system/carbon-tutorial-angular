a701ccf8361daa1d485640a905c0af9c890f1ca8
Yuri Zhukov <zhukov_yuri@ru.ibm.com>
2020-12-29 16:29:18 +0300

    feat(landing-page): content added

--- src/app/home/landing-page/landing-page.component.spec.ts
+++ src/app/home/landing-page/landing-page.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { GridModule, BreadcrumbModule } from 'carbon-components-angular';
 import { LandingPageComponent } from './landing-page.component';
 
 describe('LandingPageComponent', () => {
