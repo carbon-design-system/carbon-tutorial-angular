5cbcbee2c9da328c422010928e21d5c0235b2f57
Nathaniel Malinowski <nathaniel.malinowski@merge.com>
2021-01-13 13:17:35 -0600

    feat(tutorial): complete step 1

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,7 +1,15 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
-import { UIShellModule } from 'carbon-components-angular';
+import { UIShellModule } from 'carbon-components-angular/ui-shell/ui-shell.module';
+import { Notification20Module } from '@carbon/icons-angular/lib/notification/20';
+import { UserAvatar20Module } from '@carbon/icons-angular/lib/user--avatar/20';
+import { AppSwitcher20Module } from '@carbon/icons-angular/lib/app-switcher/20';
+
+TestBed.configureTestingModule({
+	declarations: [HeaderComponent],
+	imports: [UIShellModule]
+});
 
 describe('HeaderComponent', () => {
 	let component: HeaderComponent;
@@ -10,7 +18,7 @@ describe('HeaderComponent', () => {
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
 			declarations: [ HeaderComponent ],
-			imports: [ UIShellModule ]
+			imports: [ UIShellModule, Notification20Module, UserAvatar20Module, AppSwitcher20Module ]
 		})
 		.compileComponents();
 	}));
