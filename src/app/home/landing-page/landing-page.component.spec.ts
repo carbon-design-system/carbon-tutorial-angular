13c0df1cb8282cefcb744a09bbcfea4d9d334cd5
DanishMirrani <danishmirrani@gmail.com>
2021-01-14 16:06:35 +0500

    feat(tutorial): complete step 2

--- src/app/home/landing-page/landing-page.component.spec.ts
+++ src/app/home/landing-page/landing-page.component.spec.ts
@@ -1,5 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import {
+	BreadcrumbModule,
+	ButtonModule,
+	GridModule,
+	TabsModule,
+	} from 'carbon-components-angular';
 import { LandingPageComponent } from './landing-page.component';
 
 describe('LandingPageComponent', () => {
@@ -8,7 +13,13 @@ describe('LandingPageComponent', () => {
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
-			declarations: [ LandingPageComponent ]
+			declarations: [ LandingPageComponent ],
+			imports: [
+				BreadcrumbModule,
+				ButtonModule,
+				GridModule,
+				TabsModule
+			]
 		})
 		.compileComponents();
 	}));
