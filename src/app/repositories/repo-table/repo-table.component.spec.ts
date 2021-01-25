819bd9beb2c27de3e7ce33018cea9cd468e1656f
DanishMirrani <danishmirrani@gmail.com>
2021-01-25 09:53:23 +0500

    feat(tutorial): complete step 3

--- src/app/repositories/repo-table/repo-table.component.spec.ts
+++ src/app/repositories/repo-table/repo-table.component.spec.ts
@@ -2,16 +2,19 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RepoTableComponent } from './repo-table.component';
 import { TableModule } from 'carbon-components-angular';
+import { LinkModule, PaginationModule } from 'carbon-components-angular';
 
 describe('RepoTableComponent', () => {
 	let component: RepoTableComponent;
 	let fixture: ComponentFixture<RepoTableComponent>;
-
+	imports: [LinkModule, PaginationModule];
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
 			declarations: [ RepoTableComponent ],
 			imports: [
-				TableModule
+				TableModule,
+				LinkModule,
+				PaginationModule
 			]
 		})
 		.compileComponents();
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,6 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-	production: false
+	production: false,
+	githubPersonalAccessToken: '8ceab13753a42a487eaaa23484160c7dd8c773c6',
 };
