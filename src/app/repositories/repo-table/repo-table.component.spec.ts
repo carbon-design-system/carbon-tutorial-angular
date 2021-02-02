8a30479363c8567baa9fc9b7432e4effd14d18b4
Ye Chuan Wang <bjwangyc@cn.ibm.com>
2021-02-02 21:57:49 +0800

    feat(tutorial): complete step 2

--- src/app/repositories/repo-table/repo-table.component.spec.ts
+++ src/app/repositories/repo-table/repo-table.component.spec.ts
@@ -1,22 +1,21 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { GridModule, TableModule } from 'carbon-components-angular';
-import { RepoPageComponent } from './repo-page.component';
 import { RepoTableComponent } from '../repo-table/repo-table.component';
+import { GridModule, TableModule } from 'carbon-components-angular';
 
-describe('RepoPageComponent', () => {
-	let component: RepoPageComponent;
-	let fixture: ComponentFixture<RepoPageComponent>;
+describe('RepoTableComponent', () => {
+	let component: RepoTableComponent;
+	let fixture: ComponentFixture<RepoTableComponent>;
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
-			declarations: [ RepoPageComponent, RepoTableComponent ],
-			imports : [GridModule, TableModule]
+			declarations: [ RepoTableComponent ],
+			imports: [GridModule, TableModule]
 		})
 		.compileComponents();
 	}));
 
 	beforeEach(() => {
-		fixture = TestBed.createComponent(RepoPageComponent);
+		fixture = TestBed.createComponent(RepoTableComponent);
 		component = fixture.componentInstance;
 		fixture.detectChanges();
 	});
