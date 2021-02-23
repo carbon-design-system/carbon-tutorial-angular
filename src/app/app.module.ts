39e8844b4be2f6d07b445e6d66e5b3594b3299e0
ANAS BENZGUIL <anas.benzguil@ibm.com>
2021-02-23 14:19:15 +0000

    feat(tutorial): complete step 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,11 @@ import { UIShellModule } from 'carbon-components-angular';
 import { Notification20Module } from '@carbon/icons-angular/lib/notification/20';
 import { UserAvatar20Module } from '@carbon/icons-angular/lib/user--avatar/20';
 import { AppSwitcher20Module } from '@carbon/icons-angular/lib/app-switcher/20';
-import { HeaderComponent } from './header/header.component';
 import { CatalogComponent } from './pages/catalog/catalog.component';
 import { DocsComponent } from './pages/docs/docs.component';
 import { SupportComponent } from './pages/support/support.component';
 import { Link1Component } from './pages/link1/link1.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
 	declarations: [
