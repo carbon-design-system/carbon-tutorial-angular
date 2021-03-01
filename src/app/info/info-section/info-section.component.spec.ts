21b0a3ed438736f32db0622070e581dfdb69f214
Ye Chuan Wang <wangyechuan2008@163.com>
2021-03-01 20:51:39 +0800

    feat(tutorial): complete step 4

--- src/app/info/info-section/info-section.component.spec.ts
+++ src/app/info/info-section/info-section.component.spec.ts
@@ -1,4 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Application32Module } from '@carbon/icons-angular/lib/application/32';
+import { Globe32Module } from '@carbon/icons-angular/lib/globe/32';
+import { PersonFavorite32Module } from '@carbon/icons-angular/lib/person--favorite/32';
+import { GridModule } from 'carbon-components-angular';
+import { InfoCardComponent } from '../info-card/info-card.component';
 
 import { InfoSectionComponent } from './info-section.component';
 
@@ -8,7 +14,14 @@ describe('InfoSectionComponent', () => {
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
-			declarations: [ InfoSectionComponent ]
+			declarations: [ InfoSectionComponent, InfoCardComponent ],
+			imports: [
+				CommonModule,
+				GridModule,
+				PersonFavorite32Module,
+				Globe32Module,
+				Application32Module,
+			],
 		})
 		.compileComponents();
 	}));
