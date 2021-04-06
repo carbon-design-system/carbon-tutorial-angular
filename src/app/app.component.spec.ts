4caffa3e2b7968721cf2ab55a2b91ab0f2d61aed
Pascal Magnard <pascal.magnard@fr.ibm.com>
2021-04-06 14:25:47 +0200

    feat(tutorial): complete step 1

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,30 @@
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { UIShellModule } from 'carbon-components-angular/ui-shell/ui-shell.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+
+import Notification16 from '@carbon/icons/es/notification/16';
+import UserAvatar16 from '@carbon/icons/es/user--avatar/16';
+import AppSwitcher16 from '@carbon/icons/es/app-switcher/16';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        UIShellModule
+      ],
+      declarations: [
+        AppComponent,
+        HeaderComponent
+      ],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+});
