e8c2abde2d6f28d3bd3fee6635d6e2aa3dac4e53
gbodelon <guilhermebodelon@hotmail.com>
2021-04-23 02:06:02 -0300

    feat (tutorial): complete step 1

--- carbon-angular-tutorial/e2e/src/app.po.ts
+++ carbon-angular-tutorial/e2e/src/app.po.ts
@@ -6,6 +6,7 @@ export class AppPage {
   }
 
   async getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText();
+    return element(by.css('app-root p')).getText();
   }
+
 }
