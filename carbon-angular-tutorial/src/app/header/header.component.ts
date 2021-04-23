4ff1b1117ed90e560d57e8a2308ac974fb71d0ed
gbodelon <guilhermebodelon@hotmail.com>
2021-04-23 00:55:53 -0300

    feat (tutorial): complete step 1

---
+++ carbon-angular-tutorial/src/app/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, HostBinding } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+  constructor() { }
+  @HostBinding('class.bx--header') headerClass = true;
+
+  ngOnInit(): void {
+  }
+
+}
