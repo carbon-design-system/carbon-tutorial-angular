186ee7257142a450db16c31aee119ca1e2a96189
Isaque Galdino <isaque@br.ibm.com>
2021-04-29 19:40:44 -0300

    feat(tutorial): complete step 2

--- src/app/home/landing-page/landing-page.component.spec.ts
+++ src/app/home/landing-page/landing-page.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { BreadcrumbModule, ButtonModule, GridModule, TabsModule } from 'carbon-components-angular';
 
 import { LandingPageComponent } from './landing-page.component';
 
