f958566ee7c4b7689eb78b06640f4fb07b97de30
VaibhavRangare999 <VaibhavRangare999@gmail.com>
2021-05-04 12:45:44 +0530

    feat(tutorial): complete step 2

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,8 @@
 import { Component, HostBinding } from '@angular/core';
+import { IconService } from 'carbon-components-angular';
+import Notification20 from '@carbon/icons/es/notification/20';
+import UserAvatar20 from '@carbon/icons/es/user--avatar/20';
+import AppSwitcher20 from '@carbon/icons/es/app-switcher/20';
 
 @Component({
 	selector: 'app-header',
@@ -8,4 +12,11 @@ import { Component, HostBinding } from '@angular/core';
 export class HeaderComponent {
 	// adds padding to the top of the document, so the content is below the header
 	@HostBinding('class.bx--header') headerClass = true;
+	constructor(protected iconservice: IconService) {
+		iconservice.registerAll([
+		  Notification20,
+		  UserAvatar20,
+		  AppSwitcher20
+		]);
+	  }
 }
