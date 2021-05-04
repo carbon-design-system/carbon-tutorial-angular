7f6de5f9b3cb795f1437cce18ab4bf4fad1146d5
VaibhavRangare999 <VaibhavRangare999@gmail.com>
2021-05-04 11:25:23 +0530

    feat(tutorial): complete step 1

---
+++ src/app/header/header.component.spec.ts
@@ -0,0 +1,34 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HeaderComponent } from './header.component';
+// import { UIShellModule } from 'carbon-components-angular/ui-shell/ui-shell.module';
+import { UIShellModule } from 'carbon-components-angular';
+
+TestBed.configureTestingModule({
+  declarations: [HeaderComponent],
+  imports: [UIShellModule]
+});
+
+describe('HeaderComponent', () => {
+  let component: HeaderComponent;
+  let fixture: ComponentFixture<HeaderComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ HeaderComponent ],
+      imports: [
+        UIShellModule
+      ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HeaderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
