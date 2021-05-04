5f5b63021ddb673a2c3d41fc499ffba4f77dc835
VaibhavRangare999 <VaibhavRangare999@gmail.com>
2021-05-04 12:08:11 +0530

    feat(tutorial): complete step 2

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,5 +1,4 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HeaderComponent } from './header.component';
 import { UIShellModule } from 'carbon-components-angular';
 
