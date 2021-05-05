f8ee59216ae7104d85ba838ce27f079f5aa55a7c
Luiza Mendes <luizamendes@Luizas-MacBook-Pro.local>
2021-05-05 15:34:24 -0300

    feat(tutorial): complete step 1

--- carbon-angular-tutorial/src/app/header/header.component.spec.ts
+++ carbon-angular-tutorial/src/app/header/header.component.spec.ts
@@ -1,10 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
-import Notification16 from '@carbon/icons/es/notification/16';
-import UserAvatar16 from '@carbon/icons/es/user--avatar/16';
-import AppSwitcher16 from '@carbon/icons/es/app-switcher/16';
-import { UIShellModule } from 'carbon-components-angular/ui-shell/ui-shell.module';
+import { UIShellModule } from 'carbon-components-angular';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -12,10 +9,9 @@ describe('HeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ],
-      imports: [UIShellModule]
-    })
-    .compileComponents();
+      declarations: [HeaderComponent],
+      imports: [UIShellModule],
+    }).compileComponents();
   });
 
   beforeEach(() => {
