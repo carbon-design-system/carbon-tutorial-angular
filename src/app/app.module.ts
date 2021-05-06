99afff71af1ed3b5ef7581bf15f6235494277125
Vamsi Ambati <ambati.vamsi96@gmail.com>
2021-05-06 13:50:49 +0530

    feat(tutorial): complete step 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 // carbon-components-angular default imports
-import { UIShellModule } from 'carbon-components-angular';
-import { Notification20Module } from '@carbon/icons-angular/lib/notification/20';
-import { UserAvatar20Module } from '@carbon/icons-angular/lib/user--avatar/20';
-import { AppSwitcher20Module } from '@carbon/icons-angular/lib/app-switcher/20';
+import { UIShellModule, IconModule } from 'carbon-components-angular';
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
@@ -23,9 +20,7 @@ import { HeaderComponent } from './header/header.component';
 		FormsModule,
 		AppRoutingModule,
 		UIShellModule,
-		Notification20Module,
-		UserAvatar20Module,
-		AppSwitcher20Module
+		IconModule
 	],
 	bootstrap: [AppComponent]
 })
--- src/app/repositories/repositories.module.ts
+++ src/app/repositories/repositories.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { RepositoriesRoutingModule } from './repositories-routing.module';
 import { RepoPageComponent } from './repo-page/repo-page.component';
+import { GridModule, TableModule } from 'carbon-components-angular';
+import { RepoTableComponent } from './repo-table/repo-table.component';
 
 @NgModule({
-	declarations: [RepoPageComponent],
+	declarations: [RepoPageComponent, RepoTableComponent],
 	imports: [
 		CommonModule,
-		RepositoriesRoutingModule
+		RepositoriesRoutingModule,
+		GridModule,
+		TableModule
 	]
 })
 export class RepositoriesModule { }
