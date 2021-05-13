e59385d366756ea1ec46a9be9ca9744cf191f247
Kumaran R <kumaran@Kumarans-MacBook-Pro.local>
2021-05-13 13:24:37 +0530

    feat(tutorial): complete step 1

---
+++ carbon-angular-tutorial/src/app/app.component.spec.ts
@@ -0,0 +1,25 @@
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { UIShellModule } from 'carbon-components-angular';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule, UIShellModule
+      ],
+      declarations: [
+        AppComponent, HeaderComponent
+      ],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+});
