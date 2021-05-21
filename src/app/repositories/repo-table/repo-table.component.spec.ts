e37cc85ea7df62c75dc5932a3456a8734a86531e
Anjana M K <anjanam2@in.ibm.com>
2021-05-22 00:19:52 +0530

    feat(tutorial): complete step 3

--- src/app/repositories/repo-table/repo-table.component.spec.ts
+++ src/app/repositories/repo-table/repo-table.component.spec.ts
@@ -1,7 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RepoTableComponent } from './repo-table.component';
-import { TableModule } from 'carbon-components-angular';
+import { TableModule, LinkModule, PaginationModule } from 'carbon-components-angular';
+import { Apollo } from 'apollo-angular';
+
+class mockApollo {
+	watchQuery = [];
+  }
 
 describe('RepoTableComponent', () => {
 	let component: RepoTableComponent;
@@ -11,8 +16,12 @@ describe('RepoTableComponent', () => {
 		TestBed.configureTestingModule({
 			declarations: [ RepoTableComponent ],
 			imports: [
-				TableModule
-			]
+				TableModule,
+				LinkModule,
+				PaginationModule
+			],
+			providers: [{ provide: Apollo, useClass: mockApollo }],
+			schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA]
 		})
 		.compileComponents();
 	}));
