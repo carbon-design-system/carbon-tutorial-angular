bdfa8398163d9ab15c76560542e5cb0c8d84fc58
Sarath TD <sarathtd@Saraths-MacBook-Pro.local>
2021-05-29 00:31:29 +0530

    feat(tutorial): complete step 4

--- src/app/info/info-section/info-section.component.spec.ts
+++ src/app/info/info-section/info-section.component.spec.ts
@@ -1,25 +1,27 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule, NO_ERRORS_SCHEMA, SimpleChange, SimpleChanges } from '@angular/core';
 import { InfoSectionComponent } from './info-section.component';
+import * as data from '../info.json';
 
 describe('InfoSectionComponent', () => {
-  let component: InfoSectionComponent;
-  let fixture: ComponentFixture<InfoSectionComponent>;
+	let component: InfoSectionComponent;
+	let fixture: ComponentFixture<InfoSectionComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ InfoSectionComponent ]
-    })
-    .compileComponents();
-  }));
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [ InfoSectionComponent ],
+			schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
+		})
+		.compileComponents();
+	}));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(InfoSectionComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(InfoSectionComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
--- src/app/info/info-section/info-section.component.ts
+++ src/app/info/info-section/info-section.component.ts
@@ -2,16 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import * as data from '../info.json';
 
 @Component({
-  selector: 'app-info-section',
-  templateUrl: './info-section.component.html',
-  styleUrls: ['./info-section.component.scss']
+	selector: 'app-info-section',
+	templateUrl: './info-section.component.html',
+	styleUrls: ['./info-section.component.scss']
 })
 export class InfoSectionComponent implements OnInit {
-  heading = data.title;
-  items = data.items;
+	heading = data.title;
+	items = data.items;
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {}
+	ngOnInit() {}
 
 }
