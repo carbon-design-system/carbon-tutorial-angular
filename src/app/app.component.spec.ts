fb47edd5c0c601560baf62e9d29cabaf684d8477
Pabitra Kumar Muduli <pamudul1@in.ibm.com>
2021-05-31 11:45:29 +0530

    feat(tutorial): complete step 1

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,42 @@
+import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+import { UIShellModule } from 'carbon-components-angular/ui-shell/ui-shell.module';
+import { HeaderComponent } from './header/header.component';
+
+describe('AppComponent', () => {
+
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        UIShellModule
+      ],
+      declarations: [
+        AppComponent,
+        HeaderComponent
+      ],
+
+    }).compileComponents();
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'carbon-angular-tutorial'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('carbon-angular-tutorial');
+  });
+
+  it('should render title in a h1 tag', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to carbon-angular-tutorial!');
+  });
+});
---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { UIShellModule, IconModule } from 'carbon-components-angular';
+
+//icons
+import { Notification20Module } from '@carbon/icons-angular/lib/notification/20';
+import { UserAvatar20Module } from '@carbon/icons-angular/lib/user--avatar/20';
+import { AppSwitcher20Module } from '@carbon/icons-angular/lib/app-switcher/20';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    UIShellModule,
+    IconModule,
+    Notification20Module,
+    UserAvatar20Module,
+    AppSwitcher20Module
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
