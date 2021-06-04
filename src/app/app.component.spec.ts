411041fac567e1fe42ed4e17531f419825858986
Hui HU Zhang <zzzhhui@cn.ibm.com>
2021-06-04 10:46:43 +0800

    feat(tutorial): complete step 1

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,33 @@
+import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { UIShellModule } from 'carbon-components-angular';
+import Notification16 from '@carbon/icons/es/notification/16';
+import UserAvatar16 from '@carbon/icons/es/user--avatar/16';
+import AppSwitcher16 from '@carbon/icons/es/app-switcher/16';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+
+describe('AppComponent', () => {
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [
+				AppComponent,
+				HeaderComponent
+			],
+			imports: [
+				RouterTestingModule,
+				UIShellModule,
+				Notification16,
+				UserAvatar16,
+				AppSwitcher16
+			]
+		}).compileComponents();
+	}));
+
+	it('should create the app', async(() => {
+		const fixture = TestBed.createComponent(AppComponent);
+		const app = fixture.debugElement.componentInstance;
+		expect(app).toBeTruthy();
+	}));
+});
---
+++ src/app/repositories/repo-page/repo-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-repo-page',
+	templateUrl: './repo-page.component.html',
+	styleUrls: ['./repo-page.component.scss']
+})
+export class RepoPageComponent {
+}
