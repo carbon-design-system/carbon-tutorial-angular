8c4c880ab010572b2fe158a4614ca4d1783d7c45
Archana <arcar187@in.ibm.com>
2021-06-09 16:36:00 +0530

    feat(tutorial): complete step 4

--- src/app/info/info-card/info-card.component.ts
+++ src/app/info/info-card/info-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
 	selector: 'app-info-card',
@@ -6,20 +6,19 @@ import { Component, Input, OnInit } from '@angular/core';
 	styleUrls: ['./info-card.component.scss']
 })
 export class InfoCardComponent implements OnInit {
-	@Input() heading;
-	@Input() content;
-	splitHeading;
-	constructor() { }
+	@Input() heading = '';
+	@Input() content = '';
+	splitHeading = [];
 
 	ngOnInit() {
-		   // create the split title to get bold styles
-		   this.splitHeading = this.createArrayFromPhrase(this.heading);
+		// create the split title to get bold styles
+		this.splitHeading = this.createArrayFromPhrase(this.heading);
 	}
+
 	// Take in a phrase and separate the third word in an array
 	createArrayFromPhrase(phrase) {
-		const splitPhrase = phrase.split(" ");
+		const splitPhrase = phrase.split(' ');
 		const thirdWord = splitPhrase.pop();
-		return [splitPhrase.join(" "), thirdWord];
-	  }
-
+		return [splitPhrase.join(' '), thirdWord];
+	}
 }
