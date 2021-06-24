582f8cd8b17ca8d6688de86392887987ce0ef70c
Tom Nelson <nelsonts@us.ibm.com>
2021-06-24 16:45:28 -0500

    feat(tutorial): complete step 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => 
+    loadChildren: () =>
       import('./home/home.module').then(
         (m) => m.HomeModule
       ),
