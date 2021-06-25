c10158bff778db92517459cd5990e10c33a2fdea
Derick <dealebd@gmail.com>
2021-06-24 21:30:47 -0600

    feat(tutorial): complete step 1

---
+++ carbon-angular-tutorial/src/app/app.component.spec.ts
@@ -0,0 +1,42 @@
+import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+import { UIShellModule } from 'carbon-components-angular/ui-shell/ui-shell.module';
+import { HeaderComponent } from './header/header.component';
+
+import Notification16 from '@carbon/icons/es/notification/16';
+import UserAvatar16 from '@carbon/icons/es/user--avatar/16';
+import AppSwitcher16 from '@carbon/icons/es/app-switcher/16';
+
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
+    }).compileComponents();
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  TestBed.configureTestingModule({
+    declarations: [HeaderComponent],
+    imports: [UIShellModule],
+  });
+
+  // it(`should have as title 'carbon-angular-tutorial'`, () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.debugElement.componentInstance;
+  //   expect(app.title).toEqual('carbon-angular-tutorial');
+  // });
+
+  // it('should render title', () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.debugElement.nativeElement;
+  //   expect(compiled.querySelector('.content span').textContent).toContain('carbon-angular-tutorial app is running!');
+  // });
+});
