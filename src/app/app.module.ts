dd23315e1cbfd8aa6aa10c2c7388d45ffd25b291
Derick Blanco <derick.blanco@ibm.com>
2021-06-30 11:31:18 -0600

    Changes to get the icons to show as expected.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,45 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
 
 import {
   UIShellModule,
   IconModule,
   IconService,
-} from 'carbon-components-angular';
-import Notification16 from '@carbon/icons/es/notification/16';
-import UserAvatar16 from '@carbon/icons/es/user--avatar/16';
-import AppSwitcher16 from '@carbon/icons/es/app-switcher/16';
+} from "carbon-components-angular";
+// import Notification16 from "@carbon/icons/es/notification/16";
+// import UserAvatar16 from "@carbon/icons/es/user--avatar/16";
+// import AppSwitcher16 from "@carbon/icons/es/app-switcher/16";
+
+import {
+  NotificationModule,
+  UserAvatarModule,
+  AppSwitcherModule,
+} from "@carbon/icons-angular";
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
-  imports: [BrowserModule, AppRoutingModule, UIShellModule, IconModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    UIShellModule,
+    IconModule,
+    NotificationModule,
+    UserAvatarModule,
+    AppSwitcherModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {
-  constructor(protected iconService: IconService) {
-    iconService.registerAll([Notification16, UserAvatar16, AppSwitcher16]);
-  }
+  // constructor(protected iconService: IconService) {
+  //   iconService.registerAll([
+  //     NotificationModule,
+  //     UserAvatarModule,
+  //     AppSwitcherModule,
+  //   ]);
+  // }
 }
