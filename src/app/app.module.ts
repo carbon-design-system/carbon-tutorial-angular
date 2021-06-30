bfa1231bbed91b4966c3e0089e4fccaae63486a3
Derick Blanco <derick.blanco@ibm.com>
2021-06-30 12:26:09 -0600

    Changes in Icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { HeaderComponent } from "./header/header.component";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 import {
   UIShellModule,
   IconModule,
-  IconService,
-} from "carbon-components-angular";
+  // IconService,
+} from 'carbon-components-angular';
 // import Notification16 from "@carbon/icons/es/notification/16";
 // import UserAvatar16 from "@carbon/icons/es/user--avatar/16";
 // import AppSwitcher16 from "@carbon/icons/es/app-switcher/16";
@@ -18,7 +18,7 @@ import {
   NotificationModule,
   UserAvatarModule,
   AppSwitcherModule,
-} from "@carbon/icons-angular";
+} from '@carbon/icons-angular';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
