4e199492c0a5c3f91e95472d7a9a69e3339cb18b
Derick Blanco <derick.blanco@ibm.com>
2021-06-30 12:50:57 -0600

    Changes to run test

--- karma.conf.js
+++ karma.conf.js
@@ -29,7 +29,7 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ["ChromeHeadless"],
-    singleRun: false,
+    singleRun: true,
     restartOnFileChange: true,
   });
 };
