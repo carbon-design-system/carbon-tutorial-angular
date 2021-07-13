29b7d085d9738e614b42b4949c297f88ee25e4bc
GabrielSouzaWebGit <gabrielsouzawebgit@gmail.com>
2021-07-13 18:21:51 -0300

    feat(tutorial): complete step 1

---
+++ carbon-angular-tutorial/src/app/app.component.spec.ts
@@ -0,0 +1,30 @@
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+import { UIShellModule } from 'carbon-components-angular/ui-shell/ui-shell.module';
+import { HeaderComponent } from './header/header.component';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  TestBed.configureTestingModule({
+    declarations: [HeaderComponent],
+    imports: [UIShellModule]
+  });
+
+});
---
+++ carbon-angular-tutorial/src/app/repositories/repo-page/repo-page.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-repo-page',
+  templateUrl: './repo-page.component.html',
+  styleUrls: ['./repo-page.component.scss']
+})
+export class RepoPageComponent implements OnInit {
+
+  constructor() { 
+
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit');
+  }
+
+}
