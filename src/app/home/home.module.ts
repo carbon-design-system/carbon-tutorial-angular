320c2967dbed815df54567ea55ad8d93ab4b71ed
leaucimportant <39760521+leaucimportant@users.noreply.github.com>
2021-07-19 22:56:44 +0200

    feat(tutorial): complete step 4

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,14 +1,18 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { InfoModule } from "./../info/info.module";
+import { HomeRoutingModule } from "./home-routing.module";
+import { LandingPageComponent } from "./landing-page/landing-page.component";
+import { PersonFavorite32Module } from "@carbon/icons-angular/lib/person--favorite/32";
+import { Globe32Module } from "@carbon/icons-angular/lib/globe/32";
+import { Application32Module } from "@carbon/icons-angular/lib/application/32";
 
-import { HomeRoutingModule } from './home-routing.module';
-import { LandingPageComponent } from './landing-page/landing-page.component';
 import {
 	BreadcrumbModule,
 	ButtonModule,
 	GridModule,
-	TabsModule
-} from 'carbon-components-angular';
+	TabsModule,
+} from "carbon-components-angular";
 
 @NgModule({
 	declarations: [LandingPageComponent],
@@ -18,7 +22,11 @@ import {
 		BreadcrumbModule,
 		ButtonModule,
 		GridModule,
-		TabsModule
-	]
+		TabsModule,
+		InfoModule,
+		PersonFavorite32Module,
+		Globe32Module,
+		Application32Module,
+	],
 })
-export class HomeModule { }
+export class HomeModule {}
---
+++ src/app/info/info-card/info-card.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+	selector: 'app-info-card',
+	templateUrl: './info-card.component.html',
+	styleUrls: ['./info-card.component.scss'],
+})
+export class InfoCardComponent implements OnInit {
+	@Input() heading;
+	@Input() content;
+	splitHeading;
+
+	constructor() {}
+
+	ngOnInit() {
+		this.splitHeading = this.createArrayFromPhrase(this.heading);
+	}
+	createArrayFromPhrase(phrase) {
+		const splitPhrase = phrase.split(' ');
+		const thirdWord = splitPhrase.pop();
+		return [splitPhrase.join(' '), thirdWord];
+	}
+}
