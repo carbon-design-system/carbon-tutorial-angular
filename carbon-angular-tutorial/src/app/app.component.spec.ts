2535b4cb904c2227d92010cd675a175af3bea23f
Jason Vennin-ISC France <jason.vennin-isc.france@ibm.com>
2021-08-03 14:25:55 +0200

    feat(tutorial):complete step 1

---
+++ carbon-angular-tutorial/src/app/app.component.spec.ts
@@ -0,0 +1,33 @@
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+import { UIShellModule } from 'carbon-components-angular/ui-shell/ui-shell.module';
+import { HeaderComponent } from './header/header.component';
+import { NotificationModule } from 'carbon-components-angular';
+import { UserAvatarModule } from '@carbon/icons-angular';
+import { AppSwitcherModule } from '@carbon/icons-angular';
+
+declarations: [HeaderComponent];
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  });
+  TestBed.configureTestingModule({
+    declarations: [HeaderComponent],
+    imports: [UIShellModule]
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+});
---
+++ carbon-angular-tutorial/src/app/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { HostBinding } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+  
+  @HostBinding('class.bx--header') headerClass = true;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
