3e095f1559489a86961aaa0f1d72cb884eb1e8d6
Jason Vennin-ISC France <jason.vennin-isc.france@ibm.com>
2021-08-04 10:57:02 +0200

    feat(tutorial): complete step 3

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,6 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-	production: false
+	production: false,
+	githubPersonalAccessToken: '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',
 };
