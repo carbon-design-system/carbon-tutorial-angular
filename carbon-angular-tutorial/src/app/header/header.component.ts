3a3b74e6fcd0a33b4bea709646214938dfa7f680
Tanmoy Bhadra <tanmoy.bhadra@ibm.com>
2021-09-02 00:12:49 +0530

    feat(tutorial): complete step 1

---
+++ carbon-angular-tutorial/src/app/header/header.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, HostBinding } from '@angular/core';
+
+import Notification16 from '@carbon/icons/es/notification/16';
+import UserAvatar16 from '@carbon/icons/es/user--avatar/16';
+import AppSwitcher16 from '@carbon/icons/es/app-switcher/16';
+import { IconService } from 'carbon-components-angular';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  @HostBinding('class.bx--header') headerClass = true;
+
+  constructor(protected iconService: IconService) {
+    iconService.registerAll([
+      Notification16,
+      UserAvatar16,
+      AppSwitcher16
+    ]);
+  }
+
+  ngOnInit(): void {
+  }
+
+}
