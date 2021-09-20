4e999cb526ce946a4b9edcbc734f405331d2e343
Madalina Toma <Madalina.Toma@ibm.com>
2021-09-20 17:51:23 +0200

    feat(tutorial): complete step 1
    feat(tutorial): complete step 1

---
+++ carbon-angular-tutorial/src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+// carbon-components-angular default imports
+import { IconModule, IconService, UIShellModule } from 'carbon-components-angular';
+//import Notification20 from 'node_modules/@carbon/icons/es/notification/20';
+//const Notification20 = require('@carbon/icons/es/notification/20')
+//import UserAvatar20 from 'node_modules/@carbon/icons/es/user--avatar/20';
+//const UserAvatar20 = require('@carbon/icons/es/user--avatar/20')
+//import AppSwitcher20 from 'node_modules/@carbon/icons/es/app-switcher/20';
+//const AppSwitcher20 = require('@carbon/icons/es/app-switcher/20')
+import { HeaderComponent } from './header/header.component';
+
+
+@NgModule({
+	declarations: [
+		AppComponent,
+		HeaderComponent
+	],
+	imports: [
+		BrowserModule,
+		BrowserAnimationsModule,
+		FormsModule,
+		AppRoutingModule,
+		UIShellModule,
+		IconModule
+	],
+	bootstrap: [AppComponent]
+})
+export class AppModule {
+	constructor(protected iconService: IconService) {
+		iconService.registerAll([
+			//Notification20,
+			//UserAvatar20,
+			//AppSwitcher20
+		]);
+	}
+}
