4e1cb506a8a91ec6ee8d3f12445c18c124780f27
Madalina Toma <Madalina.Toma@ibm.com>
2021-09-20 23:44:32 +0200

    feat(tutorial): complete step 1
    feat(tutorial): complete step 1

--- carbon-angular-tutorial/src/app/app.component.spec.ts
+++ carbon-angular-tutorial/src/app/app.component.spec.ts
@@ -1,35 +1,33 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+
+import { UIShellModule } from 'carbon-components-angular';
+//import { Notification20Module } from '@carbon/icons-angular/lib/notification/20';
+//import { UserAvatar20Module } from '@carbon/icons-angular/lib/user--avatar/20';
+//import { AppSwitcher20Module } from '@carbon/icons-angular/lib/app-switcher/20';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  });
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [
+				AppComponent,
+				HeaderComponent
+			],
+			imports: [
+				RouterTestingModule,
+				UIShellModule,
+				// Notification20Module,
+//UserAvatar20Module,
+//AppSwitcher20Module
+			]
+		}).compileComponents();
+	}));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'carbon-angular-tutorial'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('carbon-angular-tutorial');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('carbon-angular-tutorial app is running!');
-  });
+	it('should create the app', async(() => {
+		const fixture = TestBed.createComponent(AppComponent);
+		const app = fixture.debugElement.componentInstance;
+		expect(app).toBeTruthy();
+	}));
 });
--- carbon-angular-tutorial/src/app/app.module.ts
+++ carbon-angular-tutorial/src/app/app.module.ts
@@ -1,18 +1,39 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// carbon-components-angular default imports
+import { IconModule, IconService, UIShellModule } from 'carbon-components-angular';
+import Notification20 from '@carbon/icons/es/notification/20';
+import UserAvatar20 from '@carbon/icons/es/user--avatar/20';
+import AppSwitcher20 from '@carbon/icons/es/app-switcher/20';
+import { HeaderComponent } from './header/header.component';
+
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HeaderComponent
+	],
+	imports: [
+		BrowserModule,
+		BrowserAnimationsModule,
+		FormsModule,
+		AppRoutingModule,
+		UIShellModule,
+		IconModule
+	],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+	constructor(protected iconService: IconService) {
+		iconService.registerAll([
+			Notification20,
+			UserAvatar20,
+			AppSwitcher20
+		]);
+	}
+}
