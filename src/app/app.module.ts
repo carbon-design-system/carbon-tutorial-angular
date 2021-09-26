57cdcdafc490beb0ef2e96b762916830d193ecb5
Wing NG <climberwing@gmail.com>
2021-09-26 16:59:36 +0800

    feat(tutorial): complete step 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
-import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FormsModule } from "@angular/forms";
+import { NgModule } from "@angular/core";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
 
 // carbon-components-angular default imports
-import { UIShellModule } from 'carbon-components-angular';
-import {
-	NotificationModule,
-	UserAvatarModule,
-	AppSwitcherModule,
-} from '@carbon/icons-angular';
-import { HeaderComponent } from './header/header.component';
+import { UIShellModule } from "carbon-components-angular";
+import { Notification20Module } from "@carbon/icons-angular/lib/notification/20";
+import { UserAvatar20Module } from "@carbon/icons-angular/lib/user--avatar/20";
+import { AppSwitcher20Module } from "@carbon/icons-angular/lib/app-switcher/20";
+import { HeaderComponent } from "./header/header.component";
 
 @NgModule({
 	declarations: [AppComponent, HeaderComponent],
@@ -22,9 +20,9 @@ import { HeaderComponent } from './header/header.component';
 		FormsModule,
 		AppRoutingModule,
 		UIShellModule,
-		NotificationModule,
-		UserAvatarModule,
-		AppSwitcherModule,
+		Notification20Module,
+		UserAvatar20Module,
+		AppSwitcher20Module,
 	],
 	bootstrap: [AppComponent],
 })
