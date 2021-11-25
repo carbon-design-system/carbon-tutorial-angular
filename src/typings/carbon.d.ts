94ff504ac082df1fa9c4305f492e0394979902f4
David Chen <davidsc@tw.ibm.com>
2021-11-25 23:56:22 +0800

    feat(tutorial): complete step 1

---
+++ src/typings/carbon.d.ts
@@ -0,0 +1,5 @@
+// declare module '@carbon';
+declare module '@carbon/icons/es';
+// declare module '@carbon/icons/es/notification/16';
+// declare module '@carbon/icons/es/user--avatar/16';
+// declare module '@carbon/icons/es/app-switcher/16';
