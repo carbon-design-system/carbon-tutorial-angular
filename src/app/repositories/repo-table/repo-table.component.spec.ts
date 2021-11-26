b9d7d4f35db7191a461009253e4eca02657e81d9
David Chen <davidsc@tw.ibm.com>
2021-11-26 10:47:59 +0800

    feat(tutorial): complete step 3

--- src/app/repositories/repo-table/repo-table.component.spec.ts
+++ src/app/repositories/repo-table/repo-table.component.spec.ts
@@ -1,7 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RepoTableComponent } from './repo-table.component';
-import { TableModule } from 'carbon-components-angular';
+import { TableModule, LinkModule, PaginationModule } from 'carbon-components-angular';
+import { Apollo } from 'apollo-angular';
+import { ApolloTestingModule } from 'apollo-angular/testing';
 
 describe('RepoTableComponent', () => {
 	let component: RepoTableComponent;
@@ -10,8 +12,9 @@ describe('RepoTableComponent', () => {
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
 			declarations: [ RepoTableComponent ],
+			providers: [ Apollo ],
 			imports: [
-				TableModule
+				TableModule, LinkModule, PaginationModule, ApolloTestingModule
 			]
 		})
 		.compileComponents();
