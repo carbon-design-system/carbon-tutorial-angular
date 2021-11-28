3abd65fc0437cb994d168a06919de744421579fd
Gregorio Gonzalez <ibm@MacBook-Pro-de-Gregorio.local>
2021-11-28 16:51:36 -0600

    feat(tutorial): complete step 1

---
+++ carbon-angular-tutorial/src/app/app.component.spec.ts
@@ -0,0 +1,36 @@
+import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+// import { UIShellModule } from 'carbon-components-angular/ui-shell/ui-shell.module';
+import { UIShellModule } from 'carbon-components-angular';
+import { HeaderComponent } from './header/header.component';
+import Notification16 from '@carbon/icons/es/notification/16';
+import UserAvatar16 from '@carbon/icons/es/user--avatar/16';
+import AppSwitcher16 from '@carbon/icons/es/app-switcher/16';
+
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        UIShellModule
+      ],
+      declarations: [
+        AppComponent,
+        HeaderComponent
+      ],
+    }).compileComponents();
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'carbon-angular-tutorial'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('carbon-angular-tutorial');
+  });
+});
