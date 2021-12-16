72de6d7843e25ca5ceda08d9e1a1309e074c26e3
lixin.wang <lixin.wang@ca.ibm.com>
2021-12-15 22:26:23 -0500

    fix test

--- src/app/repositories/repo-table/repo-table.component.ts
+++ src/app/repositories/repo-table/repo-table.component.ts
@@ -29,33 +29,16 @@ export class RepoTableComponent implements OnInit {
 	constructor(private apollo: Apollo) { }
 
 	ngOnInit() {
-		this.model.data = [
-			[
-				new TableItem({data: 'Repo 1', expandedData: 'Row description'}),
-				new TableItem({data: 'Date'}),
-				new TableItem({data: 'Date'}),
-				new TableItem({data: '123'}),
-				new TableItem({data: '456'}),
-				new TableItem({data: 'Links'})
-			],
-			[
-				new TableItem({data: 'Repo 2', expandedData: 'Row description'}),
-				new TableItem({data: 'Date'}),
-				new TableItem({data: 'Date'}),
-				new TableItem({data: '123'}),
-				new TableItem({data: '456'}),
-				new TableItem({data: 'Links'})
-			],
-			[
-				new TableItem({data: 'Repo 3', expandedData: 'Row description'}),
-				new TableItem({data: 'Date'}),
-				new TableItem({data: 'Date'}),
-				new TableItem({data: '123'}),
-				new TableItem({data: '456'}),
-				new TableItem({data: 'Links'})
-			]
+		
+		this.model.header = [
+			new TableHeaderItem({data: 'Name'}),
+			new TableHeaderItem({data: 'Created'}),
+			new TableHeaderItem({data: 'Updated'}),
+			new TableHeaderItem({data: 'Open Issues'}),
+			new TableHeaderItem({data: 'Stars'}),
+			new TableHeaderItem({data: 'Links'})
 		];
-	
+		this.model.pageLength = 10;
 
 		this.apollo.watchQuery({
 			query: gql`
