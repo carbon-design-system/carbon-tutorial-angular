25efe0d9abaf5ab08a4d0899280918e9403582d9
lixin.wang <lixin.wang@ca.ibm.com>
2021-12-15 22:07:39 -0500

    fix test

--- src/app/repositories/repo-table/repo-table.component.spec.ts
+++ src/app/repositories/repo-table/repo-table.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RepoTableComponent } from './repo-table.component';
 import { TableModule } from 'carbon-components-angular';
 import { LinkModule, PaginationModule } from 'carbon-components-angular';
+import { Apollo } from 'apollo-angular';
 
 describe('RepoTableComponent', () => {
 	let component: RepoTableComponent;
@@ -11,6 +12,7 @@ describe('RepoTableComponent', () => {
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
 			declarations: [ RepoTableComponent ],
+			providers: [ Apollo ],
 			imports: [
 				TableModule, LinkModule, PaginationModule
 			]
