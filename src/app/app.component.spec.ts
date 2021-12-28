0b1be0e041ab980d77f288752420311815c8b207
Hakam <hakam.abdelqader@ibm.com>
2021-12-28 14:45:16 +0400

    feat(tutorial): complete step 1

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,11 @@
+import { UIShellModule } from 'carbon-components-angular/ui-shell/ui-shell.module';
+import { TestBed} from '@angular/core/testing';
+import { HeaderComponent } from './header/header.component';
+import Notification16 from '@carbon/icons/es/notification/16';
+import UserAvatar16 from '@carbon/icons/es/user--avatar/16';
+import AppSwitcher16 from '@carbon/icons/es/app-switcher/16';
+
+TestBed.configureTestingModule({
+  declarations: [HeaderComponent],
+  imports: [UIShellModule]
+});
