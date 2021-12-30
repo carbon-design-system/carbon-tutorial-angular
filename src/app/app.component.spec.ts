81c034924b881d524b714b362fe3f6ee47153b79
hakamaq <hakamaq1@gmail.com>
2021-12-30 19:46:28 +0400

    updated path to ui-shell-module

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,6 @@
-import { UIShellModule } from 'carbon-components-angular/ui-shell/ui-shell.module';
+import { UIShellModule } from 'carbon-components-angular/ui-shell';
 import { TestBed} from '@angular/core/testing';
 import { HeaderComponent } from './header/header.component';
-import Notification20 from '@carbon/icons/es/notification/20';
-import UserAvatar20 from '@carbon/icons/es/user--avatar/20';
-import AppSwitcher20 from '@carbon/icons/es/app-switcher/20';
 
 TestBed.configureTestingModule({
   declarations: [HeaderComponent],
