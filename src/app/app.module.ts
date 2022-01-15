6a68596a2529d41bcc75daa7360ea3db3b25fffa
YuriiSelebynka <65944267+YuriiSelebynka@users.noreply.github.com>
2022-01-15 17:48:35 +0200

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// carbon-components-angular default imports
 import { UIShellModule } from 'carbon-components-angular';
-import { Notification20Module } from '@carbon/icons-angular/lib/notification/20';
-import { UserAvatar20Module } from '@carbon/icons-angular/lib/user--avatar/20';
-import { AppSwitcher20Module } from '@carbon/icons-angular/lib/app-switcher/20';
+import { AppSwitcherModule, UserAvatarModule, NotificationModule } from "@carbon/icons-angular";
 import { HeaderComponent } from './header/header.component';
-import { GraphQLModule } from './graphql.module';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
 	declarations: [
@@ -24,11 +21,9 @@ import { HttpClientModule } from '@angular/common/http';
 		FormsModule,
 		AppRoutingModule,
 		UIShellModule,
-		Notification20Module,
-		UserAvatar20Module,
-		AppSwitcher20Module,
-		GraphQLModule,
-		HttpClientModule
+		NotificationModule,
+		UserAvatarModule,
+		AppSwitcherModule
 	],
 	bootstrap: [AppComponent]
 })
--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -1,31 +1,19 @@
-import { NgModule } from '@angular/core';
-import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
-import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpHeaders } from '@angular/common/http';
-import { environment } from '../environments/environment';
+import { InMemoryCache } from '@apollo/client';
+import { HttpLink } from 'apollo-link-http';
+import { environment } from 'src/environments/environment';
+import { createHttpLink } from 'apollo-link-http'; 
 
 const uri = 'https://api.github.com/graphql'; // <-- add the URL of the GraphQL server here
+const link = createHttpLink({ uri: "/graphql" });
 export function createApollo(httpLink: HttpLink) {
-	return {
-		link: httpLink.create({
-			uri,
-			headers: new HttpHeaders({
-				'Authorization': `Bearer ${environment.githubPersonalAccessToken}`
-			})
-		}),
-		cache: new InMemoryCache()
-	};
+  return {
+    link: httpLink.create({
+      uri,
+      headers: new HttpHeaders({
+        Authorization: `Bearer ${environment.githubPersonalAccessToken}`,
+      }),
+    }),
+    cache: new InMemoryCache(),
+  };
 }
-
-@NgModule({
-	exports: [ApolloModule, HttpLinkModule],
-	providers: [
-		{
-			provide: APOLLO_OPTIONS,
-			useFactory: createApollo,
-			deps: [HttpLink]
-		}
-	]
-})
-export class GraphQLModule {}
