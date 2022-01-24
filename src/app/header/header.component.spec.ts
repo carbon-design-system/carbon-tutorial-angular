6360fb40291edabc75f582f9696087fe04ff29cd
JonathanScialpi <scialpi27@gmail.com>
2022-01-24 14:57:57 -0500

    feat(tutorial): complete step 1

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
-import { UIShellModule } from 'carbon-components-angular/ui-shell/ui-shell.module';
+import { UIShellModule } from 'carbon-components-angular';
 import { AppSwitcherModule, UserAvatarModule, NotificationModule } from "@carbon/icons-angular";
 
 
