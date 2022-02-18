c3be4719e81aaea0ad8aebb2857288163417b233
Don Page <dgpage@ibm.com>
2022-02-18 16:14:00 -0500

    feat(tutorial): complete step 1

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, HostBinding } from '@angular/core';
+
+// carbon-components-angular default imports
+import { UIShellModule, IconModule, IconService } from 'carbon-components-angular';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+
+export class HeaderComponent implements OnInit {
+
+  @HostBinding('class.bx--header') headerClass = true;
+
+   constructor() { }
+
+  ngOnInit(): void {
+    console.log('header ngOnInit');
+  }
+
+}
