6ea2cf5abcacaaf0e8aea93f0ae1581f1e002b3e
Don Page <dgpage@ibm.com>
2022-02-21 14:44:50 -0500

    feat(tutorial): complete step 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+//import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,6 +8,7 @@ import { HeaderComponent } from './header/header.component';
 
 // carbon-components-angular default imports
 import { UIShellModule, IconModule, IconService } from 'carbon-components-angular';
+//import { UIShellModule, IconModule } from 'carbon-components-angular';
 
 // need to import carbon icons
 import Notification16 from '@carbon/icons/es/notification/16';
@@ -24,10 +26,13 @@ import AppSwitcher16 from '@carbon/icons/es/app-switcher/16';
       UIShellModule,
       IconModule
     ],
+    exports: [
+      HeaderComponent
+    ],
     providers: [IconService],
     bootstrap: [AppComponent]
     
-    ,schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
+//    ,schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
   })
 
   export class AppModule {
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, HostBinding } from '@angular/core';
 
 // carbon-components-angular default imports
-import { UIShellModule, IconModule, IconService } from 'carbon-components-angular';
+//import { UIShellModule, IconModule, IconService } from 'carbon-components-angular';
+import { UIShellModule, IconModule } from 'carbon-components-angular';
+
 
 @Component({
   selector: 'app-header',
@@ -13,7 +15,7 @@ export class HeaderComponent implements OnInit {
 
   @HostBinding('class.bx--header') headerClass = true;
 
-   constructor() { }
+  constructor() { }
 
   ngOnInit(): void {
     console.log('header ngOnInit');
