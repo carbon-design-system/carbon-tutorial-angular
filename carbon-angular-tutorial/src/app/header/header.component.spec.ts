89852c2efbc58fd17a6fdd87929b42778ac3cdf3
Takayuki Maeda <AB651711@jp.ibm.com>
2022-03-09 09:38:29 +0900

    feat(tutorial): complete step 1

---
+++ carbon-angular-tutorial/src/app/header/header.component.spec.ts
@@ -0,0 +1,45 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HeaderComponent } from './header.component';
+
+import { UIShellModule, IconService } from 'carbon-components-angular';
+import Notification16 from '@carbon/icons/es/notification/16';
+import UserAvatar16 from '@carbon/icons/es/user--avatar/16';
+import AppSwitcher16 from '@carbon/icons/es/app-switcher/16';
+
+describe('HeaderComponent', () => {
+  let component: HeaderComponent;
+  let fixture: ComponentFixture<HeaderComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ HeaderComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HeaderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  TestBed.configureTestingModule({
+    declarations: [HeaderComponent],
+    imports: [UIShellModule]
+  });
+});
+
+export class HeaderComponentSpec {
+  constructor(protected iconService: IconService) {
+    iconService.registerAll([
+      Notification16,
+      UserAvatar16,
+      AppSwitcher16
+    ]);
+  }
+}
