b1faa89ea58a0d8f0f5970ac6873534055ddf5c5
Camila Rangel <camilafariasrangel@gmail.com>
2022-03-26 18:36:58 -0300

    feat: header routes

--- carbon-angular-tutorial/src/app/home/home-routing.module.ts
+++ carbon-angular-tutorial/src/app/home/home-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LandingPageComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
