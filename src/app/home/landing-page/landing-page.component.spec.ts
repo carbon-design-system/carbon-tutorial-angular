564f6806a3489cc4ab71c5ebba4fdfcab6f512af
Arthur Nagashima <Arthur.Nagashima@ibm.com>
2022-05-16 09:31:08 -0400

    feat(tutorial): complete step 2

--- src/app/home/landing-page/landing-page.component.spec.ts
+++ src/app/home/landing-page/landing-page.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { GridModule, BreadcrumbModule, TabsModule, ButtonModule } from 'carbon-components-angular';
 
 import { LandingPageComponent } from './landing-page.component';
 
@@ -8,7 +9,13 @@ describe('LandingPageComponent', () => {
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
-			declarations: [ LandingPageComponent ]
+			declarations: [ LandingPageComponent ],
+			imports: [
+				GridModule,
+				BreadcrumbModule,
+				TabsModule,
+				ButtonModule
+			]
 		})
 		.compileComponents();
 	}));
