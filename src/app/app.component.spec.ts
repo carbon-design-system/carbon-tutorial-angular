1798f500ddb703b9437c8152f77a287baf0b815e
Arthur Nagashima <Arthur.Nagashima@ibm.com>
2022-05-27 12:37:12 -0400

    feat(tutorial): complete step 1

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,37 +1,30 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { AppSwitcherModule, NotificationModule, UserAvatarModule } from '@carbon/icons-angular';
 import { HeaderComponent } from './header/header.component';
 import { UIShellModule } from 'carbon-components-angular';
-
-
-describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule,
-        UIShellModule,
-        NotificationModule,
-        AppSwitcherModule,
-        UserAvatarModule,
-      ],
-      declarations: [
-        AppComponent,
-        HeaderComponent
-      ],
-    }).compileComponents();
+  
+  describe('AppComponent', () => {
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        declarations: [
+          AppComponent,
+          HeaderComponent
+        ],
+        imports: [
+          RouterTestingModule,
+          UIShellModule,
+          NotificationModule,
+          UserAvatarModule,
+          AppSwitcherModule
+        ]
+      }).compileComponents();
+    }));
+  
+    it('should create the app', async(() => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.debugElement.componentInstance;
+      expect(app).toBeTruthy();
+    }));
   });
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'carbon-angular-tutorial'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('carbon-angular-tutorial');
-  });
-});
