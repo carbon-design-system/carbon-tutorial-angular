25a895d1507841c3145b5b92e612d4630613fb50
Deepanshu Arora <deepanshu.arora4@ibm.com>
2022-07-01 20:24:44 +0530

    feat(tutorial): complete step 1

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,29 +1,27 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
-import { UIShellModule } from 'carbon-components-angular/ui-shell/ui-shell.module';
+import { UIShellModule } from 'carbon-components-angular';
 
-// describe('HeaderComponent', () => {
-//   let component: HeaderComponent;
-//   let fixture: ComponentFixture<HeaderComponent>;
+describe('HeaderComponent', () => {
+	let component: HeaderComponent;
+	let fixture: ComponentFixture<HeaderComponent>;
 
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       declarations: [ HeaderComponent ]
-//     })
-//     .compileComponents();
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [ HeaderComponent ],
+			imports: [ UIShellModule ]
+		})
+		.compileComponents();
+	}));
 
-//     fixture = TestBed.createComponent(HeaderComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(HeaderComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
-
-TestBed.configureTestingModule({
-  declarations: [HeaderComponent],
-  imports: [UIShellModule]
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
