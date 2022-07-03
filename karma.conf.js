4ea11227f9d3c337062fea2fcc5a922749b05811
Deepanshu Arora <deepanshu.arora4@ibm.com>
2022-07-04 02:57:19 +0530

    feat(tutorial): complete step 1

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,8 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-firefox-launcher'),
-      require('karma-phantomjs-launcher'),
+      // require('karma-phantomjs-launcher'),
+      require('karma-phantomjs2-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
