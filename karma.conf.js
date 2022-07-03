042f15fc917ea7bcb5acb06acd0353994e0d6f73
Deepanshu Arora <deepanshu.arora4@ibm.com>
2022-07-04 02:26:14 +0530

    feat(tutorial): complete step 1

--- karma.conf.js
+++ karma.conf.js
@@ -2,6 +2,7 @@
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
 module.exports = function (config) {
+  process.env.FIREFOX_BIN = require('puppeteer').executablePath();
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
