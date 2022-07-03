8533c0c5e68b2ba81b8e7e96776aa3fdd8459dbe
Deepanshu Arora <deepanshu.arora4@ibm.com>
2022-07-04 03:29:42 +0530

    feat(tutorial): complete step 1

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,7 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       // require('karma-firefox-launcher'),
-      // require('karma-phantomjs-launcher'),
+      require('karma-phantomjs-launcher'),
       // require('karma-phantomjs2-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
@@ -53,7 +53,7 @@ module.exports = function (config) {
   //         }
   //     }
   // },
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
   //     browsers: ['ChromeHeadless'],
   //   customLaunchers: {
   //     'ChromeHeadless': {
