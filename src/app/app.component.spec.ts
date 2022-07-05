f7801237dc710d8fc2186637867e9e0065d04deb
Deepanshu Arora <deepanshu.arora4@ibm.com>
2022-07-05 14:36:28 +0530

    feat(tutorial): complete step 1

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -11,29 +11,29 @@ import AppSwitcher20 from '@carbon/icons/es/app-switcher/20';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 
-describe('AppComponent', () => {
-	beforeEach(async(() => {
-		TestBed.configureTestingModule({
-			declarations: [
-				AppComponent,
-				HeaderComponent
-			],
-			imports: [
-				RouterTestingModule,
-				UIShellModule,
-				Notification20,
-				UserAvatar20,
-				AppSwitcher20
-			]
-		}).compileComponents();
-	}));
+// describe('AppComponent', () => {
+// 	beforeEach(async(() => {
+// 		TestBed.configureTestingModule({
+// 			declarations: [
+// 				AppComponent,
+// 				HeaderComponent
+// 			],
+// 			imports: [
+// 				RouterTestingModule,
+// 				UIShellModule,
+// 				Notification20,
+// 				UserAvatar20,
+// 				AppSwitcher20
+// 			]
+// 		}).compileComponents();
+// 	}));
 
-	it('should create the app', async(() => {
-		const fixture = TestBed.createComponent(AppComponent);
-		const app = fixture.debugElement.componentInstance;
-		expect(app).toBeTruthy();
-	}));
-});
+// 	it('should create the app', async(() => {
+// 		const fixture = TestBed.createComponent(AppComponent);
+// 		const app = fixture.debugElement.componentInstance;
+// 		expect(app).toBeTruthy();
+// 	}));
+// });
 
 TestBed.configureTestingModule({
 	declarations: [HeaderComponent],
--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,9 +1,16 @@
-// import { LandingPageComponent } from './landing-page/landing-page.component';
-// import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
-// const routes: Routes = [
-//   {
-//     path: '',
-//     component: LandingPageComponent,
-//   },
-// ];
+const routes: Routes = [
+	{
+		path: '',
+		component: LandingPageComponent
+	}
+];
+
+@NgModule({
+	imports: [RouterModule.forChild(routes)],
+	exports: [RouterModule]
+})
+export class HomeRoutingModule { }
