063d5b3e153c75025930020fa6d24ab926035367
Deepanshu Arora <deepanshu.arora4@ibm.com>
2022-07-06 02:03:15 +0530

    feat(tutorial): complete step 1

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+
+// carbon-components-angular default imports
+import { UIShellModule, IconModule, IconService } from 'carbon-components-angular';
+import Notification20 from '@carbon/icons/es/notification/20';
+import UserAvatar20 from '@carbon/icons/es/user--avatar/20';
+import AppSwitcher20 from '@carbon/icons/es/app-switcher/20';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    UIShellModule,
+    IconModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { 
+  constructor(protected iconService: IconService) {
+    iconService.registerAll([
+      Notification20,
+      UserAvatar20,
+      AppSwitcher20
+    ]);
+  }
+}
