0a6c456695e0dd255c368786074f39e66072101e
Deepanshu Arora <deepanshu.arora4@ibm.com>
2022-07-07 23:17:04 +0530

    feat(tutorial): complete step 3

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,6 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-	production: false
+	production: false,
+	githubPersonalAccessToken: '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'
 };
