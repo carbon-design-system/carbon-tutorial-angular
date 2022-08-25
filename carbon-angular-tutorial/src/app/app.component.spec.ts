211983c9158da81d84a4b89550116bad6d09d672
Ankit Sancheti <anksanch@in.ibm.com>
2022-08-25 18:16:00 +0530

    feat(tutorial): complete step 1

---
+++ carbon-angular-tutorial/src/app/app.component.spec.ts
@@ -0,0 +1,33 @@
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+import { UIShellModule } from 'carbon-components-angular';
+import { HeaderComponent } from './header/header.component';
+
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        UIShellModule
+      ],
+      declarations: [
+        AppComponent,
+        HeaderComponent
+      ],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'carbon-angular-tutorial'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('carbon-angular-tutorial');
+  });
+});
