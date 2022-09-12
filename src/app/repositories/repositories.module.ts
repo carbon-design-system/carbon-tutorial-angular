87b5d2f64653755cc8b8a440768b10056420d12c
Akshat Patel <akshat@live.ca>
2022-09-12 16:30:48 -0400

    Generate repositories module with routing
    Signed-off-by: Akshat Patel <akshat@live.ca>

---
+++ src/app/repositories/repositories.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RepositoriesRoutingModule } from './repositories-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RepositoriesRoutingModule
+  ]
+})
+export class RepositoriesModule { }
