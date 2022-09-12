619433ca302d50d69ac6244dfaf7dae14f5dda3d
Akshat Patel <akshat@live.ca>
2022-09-12 16:31:08 -0400

    Generate repo-page component
    Signed-off-by: Akshat Patel <akshat@live.ca>

--- src/app/repositories/repositories.module.ts
+++ src/app/repositories/repositories.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { RepositoriesRoutingModule } from './repositories-routing.module';
+import { RepoPageComponent } from './repo-page/repo-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    RepoPageComponent
+  ],
   imports: [
     CommonModule,
     RepositoriesRoutingModule
