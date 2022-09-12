f08a83b99e61a4d3949ac980b95c9139b30fdf65
Akshat Patel <akshat@live.ca>
2022-09-12 16:37:18 -0400

    feat(tutorial): complete step 1

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LandingPageComponent } from './landing-page/landing-page.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LandingPageComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/repositories/repositories-routing.module.ts
+++ src/app/repositories/repositories-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { RepoPageComponent } from './repo-page/repo-page.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: RepoPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
