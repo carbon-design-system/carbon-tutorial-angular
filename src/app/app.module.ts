09207f0929003829c37c97e043738a142edd801c
Akshat Patel <akshat@live.ca>
2022-09-12 15:38:25 -0400

    Generate header component
    Signed-off-by: Akshat Patel <akshat@live.ca>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+
+// carbon-components-angular default imports
+import { UIShellModule, IconModule } from 'carbon-components-angular';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UIShellModule,
+    IconModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
