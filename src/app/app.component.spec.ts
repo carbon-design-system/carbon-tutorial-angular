7d6076085100754730926b695b073dc41a2e5b4f
Jozef Plachý <jozef.plachy@sk.ibm.com>
2022-09-13 09:26:43 +0200

    spec update

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,9 +2,6 @@ import { TestBed, async, fakeAsync } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { UIShellModule } from 'carbon-components-angular';
-import { Switcher20 } from '@carbon/icons/es/switcher/20';
-import { Notification20 } from '@carbon/icons/es/notification/20';
-import { UserAvatar20 } from '@carbon/icons/es/user--avatar/20';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -18,10 +15,7 @@ describe('AppComponent', () => {
 			],
 			imports: [
 				RouterTestingModule,
-				UIShellModule,
-				Notification20,
-				UserAvatar20,
-				Switcher20
+				UIShellModule
 			]
 		}).compileComponents();
 	}));
