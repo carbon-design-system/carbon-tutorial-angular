c92943371986f5aa8f5c882920b61d18c3b74da5
GUILLERMO DE JESUS ANDRADE GONZALEZ <guillermo.andrade@ibm.com>
2022-09-30 13:08:53 -0500

    feat(tutorial): complete step 1

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component, HostBinding, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+  @HostBinding('class.bx--header') headerClass = true;
+
+}
