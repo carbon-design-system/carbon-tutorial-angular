9b7c0f87def115e969176784ff53ae5c6daef4f7
prasadrajacode <prasadrajacode@gmail.com>
2022-10-12 12:20:56 +0800

    step 1 updated unit tests

--- karma.conf.js
+++ karma.conf.js
@@ -37,8 +37,9 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
     singleRun: false,
     restartOnFileChange: true
   });
 };
+
--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,21 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { UIShellModule } from 'carbon-components-angular';
 
 import { HeaderComponent } from './header.component';
-import { UIShellModule } from 'carbon-components-angular/ui-shell/ui-shell.module';
+
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ],
-      imports: [UIShellModule]
-    })
-    .compileComponents();
+      imports: [UIShellModule],
+      declarations: [HeaderComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 });
