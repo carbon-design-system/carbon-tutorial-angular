ed3b9015198409f68ab78469c8bf997c3076a74a
lokeshwarimurugan <Lokeshwari.Murugan@ibm.com>
2022-10-31 18:25:10 +0530

    feat(tutorial): complete step 1

---
+++ carbon-angular-tutorial/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+// carbon-components-angular default imports
+import { UIShellModule } from 'carbon-components-angular';
+
+import { HeaderComponent } from './header/header.component';
+
+
+@NgModule({
+	declarations: [
+		AppComponent,
+		HeaderComponent
+	],
+	imports: [
+		BrowserModule,
+		BrowserAnimationsModule,
+		FormsModule,
+		AppRoutingModule,
+		UIShellModule,
+	],
+	bootstrap: [AppComponent]
+})
+export class AppModule { }
