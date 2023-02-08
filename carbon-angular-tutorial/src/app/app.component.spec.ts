0c87a5107441e617866edb4aaeadf026d208a97b
sistiri <68205388+sistiri@users.noreply.github.com>
2023-02-08 21:33:02 +0100

    removing some default code parts

--- carbon-angular-tutorial/src/app/app.component.spec.ts
+++ carbon-angular-tutorial/src/app/app.component.spec.ts
@@ -20,16 +20,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'carbon-angular-tutorial'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('carbon-angular-tutorial');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('carbon-angular-tutorial app is running!');
-  });
 });
