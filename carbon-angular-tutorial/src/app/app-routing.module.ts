38bcbb0f99640eb80c87f0d7526c52617033e8fc
sistiri <68205388+sistiri@users.noreply.github.com>
2023-02-08 22:47:11 +0100

    Creating Pages (modules & routes)

--- carbon-angular-tutorial/src/app/app-routing.module.ts
+++ carbon-angular-tutorial/src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'repos',
+    loadChildren: () =>
+      import('./repositories/repositories.module').then(
+        (m) => m.RepositoriesModule
+      ),
+  },
+];
 
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule]
+// })
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
