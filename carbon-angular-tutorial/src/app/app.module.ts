9d91021752517aa95639bac4845174b4338161dd
sistiri <68205388+sistiri@users.noreply.github.com>
2023-02-08 21:45:06 +0100

    Import the Carbon UI Shell components

--- carbon-angular-tutorial/src/app/app.module.ts
+++ carbon-angular-tutorial/src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+
+// carbon-components-angular default imports
+import { UIShellModule, IconModule } from 'carbon-components-angular';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UIShellModule,
+    IconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
