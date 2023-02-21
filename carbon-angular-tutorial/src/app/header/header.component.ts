95ebbcd7a28cd6e6e1bb05810bc4e09bfb59a17d
Muthukumaran Madhiyazhagan <Muthukumaran.Madhiyazhagan@ibm.com>
2023-02-21 11:00:05 +0530

    feat(tutorial): complete step 1

---
+++ carbon-angular-tutorial/src/app/header/header.component.ts
@@ -0,0 +1,22 @@
+import { Component, HostBinding } from '@angular/core';
+import { IconService } from "carbon-components-angular";
+import Notification20 from '@carbon/icons/es/notification/20';
+import UserAvatar20 from '@carbon/icons/es/user--avatar/20';
+import AppSwitcher20 from '@carbon/icons/es/switch-layer-2/20';
+
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+  @HostBinding('class.cds--header') headerClass = true;
+  constructor(protected iconService: IconService) {}
+
+  ngOnInit() {
+    this.iconService.registerAll([Notification20]);
+    this.iconService.registerAll([UserAvatar20]);
+    this.iconService.registerAll([AppSwitcher20]);
+  }
+}
