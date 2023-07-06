d53e53c9b21906f64ace05d5ff36ccabc5f3ae3a
Ale Centeno <alejandra.centeno.alcaraz@ibm.com>
2023-07-06 16:27:58 -0600

    feat(tutorial): complete step 3

--- src/app/repositories/repo-table/repo-table.component.ts
+++ src/app/repositories/repo-table/repo-table.component.ts
@@ -29,29 +29,7 @@ export class RepoTableComponent implements OnInit {
 	skeletonModel = Table.skeletonModel(10, 6);
 	skeleton = true;
 
-	prepareData(data) {
-		this.skeleton = false; //quitar el loading
-		const newData = [];
-
-		for (const datum of data) {
-			console.log("entrando");
-			newData.push([
-				new TableItem({ data: datum.name, expandedData: datum.description }),
-				new TableItem({ data: new Date(datum.createdAt).toLocaleDateString() }),
-				new TableItem({ data: new Date(datum.updatedAt).toLocaleDateString() }),
-				new TableItem({ data: datum.issues.totalCount }),
-				new TableItem({ data: datum.stargazers.totalCount }),
-				new TableItem({
-					data: {
-						github: datum.url,
-						homepage: datum.homepageUrl
-					},
-					template: this.linkTemplate
-				})
-			]);
-		}
-		return newData;
-	}
+	data = [];
 
 	ngOnInit() {
 		this.apollo.watchQuery({
@@ -89,55 +67,54 @@ export class RepoTableComponent implements OnInit {
     }
   `
 		}).valueChanges.subscribe((response: any) => {
-  if (response.error) {
-    const errorData = [];
-    errorData.push([
-      new TableItem({data: 'error!' })
-    ]);
-    this.model.data = errorData;
-  } else if (response.loading) {
-    // Add loading state
-		this.skeleton = true;
-  } else {
-	// If we're here, we've got our data!
-	this.model.data = this.prepareData(
-  response.data.organization.repositories.nodes
-);
-  }
-});
-		/* 		this.model.data = [
-					[
-						new TableItem({data: 'Repo 1', expandedData: 'Row description'}),
-						new TableItem({data: 'Date'}),
-						new TableItem({data: 'Date'}),
-						new TableItem({data: '123'}),
-						new TableItem({data: '456'}),
-						new TableItem({data: 'Links'})
-					],
-					[
-						new TableItem({data: 'Repo 2', expandedData: 'Row description'}),
-						new TableItem({data: 'Date'}),
-						new TableItem({data: 'Date'}),
-						new TableItem({data: '123'}),
-						new TableItem({data: '456'}),
-						new TableItem({data: 'Links'})
-					],
-					[
-						new TableItem({data: 'Repo 3', expandedData: 'Row description'}),
-						new TableItem({data: 'Date'}),
-						new TableItem({data: 'Date'}),
-						new TableItem({data: '123'}),
-						new TableItem({data: '456'}),
-						new TableItem({data: 'Links'})
-					]
-				];
-				this.model.header = [
-					new TableHeaderItem({data: 'Name'}),
-					new TableHeaderItem({data: 'Created'}),
-					new TableHeaderItem({data: 'Updated'}),
-					new TableHeaderItem({data: 'Open Issues'}),
-					new TableHeaderItem({data: 'Stars'}),
-					new TableHeaderItem({data: 'Links'})
-				];*/
+			if (response.error) {
+				const errorData = [];
+				errorData.push([
+					new TableItem({ data: 'error!' })
+				]);
+				this.model.data = errorData;
+			} else if (response.loading) {
+				// Add loading state
+				this.skeleton = true;
+			} else {
+				// If we're here, we've got our data!
+				this.data = response.data.organization.repositories.nodes;
+				this.model.pageLength = 10; //initializes the page to 10 and specified in our loading skeleton
+				this.model.totalDataLength = response.data.organization.repositories.totalCount;
+				this.selectPage(1);
+			}
+		});
 	}
+
+	prepareData(data) {
+		this.skeleton = false; //quitar el loading
+		const newData = [];
+
+		for (const datum of data) {
+			console.log("entrando");
+			newData.push([
+				new TableItem({ data: datum.name, expandedData: datum.description }),
+				new TableItem({ data: new Date(datum.createdAt).toLocaleDateString() }),
+				new TableItem({ data: new Date(datum.updatedAt).toLocaleDateString() }),
+				new TableItem({ data: datum.issues.totalCount }),
+				new TableItem({ data: datum.stargazers.totalCount }),
+				new TableItem({
+					data: {
+						github: datum.url,
+						homepage: datum.homepageUrl
+					},
+					template: this.linkTemplate
+				})
+			]);
+		}
+		return newData;
+	}
+
+	selectPage(page) {
+		const offset = this.model.pageLength * (page - 1);
+		const pageRawData = this.data.slice(offset, offset + this.model.pageLength);
+		this.model.data = this.prepareData(pageRawData);
+		this.model.currentPage = page;
+	}
+
 }
