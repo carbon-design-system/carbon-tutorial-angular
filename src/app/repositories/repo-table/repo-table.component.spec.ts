0ff5ab2134da54eadd8f7cdb211fd4fcb8d1844b
BharathReddyMaram <124201941+BharathReddyMaram@users.noreply.github.com>
2023-11-10 13:35:10 -0800

    feat(tutorial): complete step 2

---
+++ src/app/repositories/repo-table/repo-table.component.spec.ts
@@ -0,0 +1,26 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RepoTableComponent } from './repo-table.component';
+import { GridModule, TableModule } from 'carbon-components-angular';
+
+describe('RepoTableComponent', () => {
+	let component: RepoTableComponent;
+	let fixture: ComponentFixture<RepoTableComponent>;
+
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [RepoTableComponent],
+			imports: [GridModule, TableModule],
+		}).compileComponents();
+	}));
+
+	beforeEach(() => {
+		fixture = TestBed.createComponent(RepoTableComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
+
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+});
